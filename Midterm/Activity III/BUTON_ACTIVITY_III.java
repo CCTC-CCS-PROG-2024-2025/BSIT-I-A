7a6823867bea56163513bba908f1520837d1c484
jeanmaica1810 <jeanbuton754@gmail.com>
2024-10-28 11:03:59 +0800

    Add files via upload

---
+++ Midterm/Activity III/BUTON_ACTIVITY_III.java
@@ -0,0 +1,16 @@
+import java.ulit.scanner;
+import java.util.Scanner;
+ class BUTON_ACTIVITY_III {
+    public static void main (String[] args) {
+    Scanner sc = new scanner(System.In);
+    System.out.println();
+    System.out.print("Enter Timer:");
+    int timer = scanner.nextInt();
+
+    while (timer >= 0 ) {
+        System.out.println(timer);
+        timer ==;
+    }
+    scanner.close();
+}
+ }
