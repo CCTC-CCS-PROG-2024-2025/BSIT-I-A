5f6af8638a879e5b57cca02642d450aacae38586
Jumar Dizon <swordego2019@gmail.com>
2024-10-28 11:16:06 +0800

    Add files via upload

---
+++ Prelim/Activity IV/DIZON_ACTIVITY_IV {.java
@@ -0,0 +1,18 @@
+
+class DIZON_ACTIVITY_IV {
+    public static void main (String[] args) {
+        int a = 17;
+        int b = 27;
+        int c = 15;
+
+        int sum = (a + b);
+        int product = (a*c);
+        int difference = (b - a);
+        int quotient = (b/c);
+        
+        System.out.println(sum);
+        System.out.println(product);
+        System.out.println(difference);
+        System.out.println(quotient);
+    }
+}
