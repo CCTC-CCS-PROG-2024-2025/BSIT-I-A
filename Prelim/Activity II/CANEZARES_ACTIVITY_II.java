fed41b9f2de9af38a451b2b3e7de9fe9c2a4c4dd
Gabriel Cañezares <astxrismfx@gmail.com>
2024-10-28 11:14:34 +0800

    Add files via upload

---
+++ Prelim/Activity II/CANEZARES_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class CANEZARES_ACTIVITY_II {
+    public static void main(String[] args) {
+        System.out.println("Gabriel Genon Canezares");
+        System.out.println(18);
+        System.out.println(27);
+        System.out.println(27 - 18);
+    }
+}
