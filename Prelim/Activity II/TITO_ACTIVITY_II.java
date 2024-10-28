12c62c3d7c1fa81899efc9d373bb14b2258b484a
Lloyd Tito <notlloydtito@gmail.com>
2024-10-28 11:18:49 +0800

    Add files via upload

---
+++ Prelim/Activity II/TITO_ACTIVITY_II.java
@@ -0,0 +1,12 @@
+/**
+ * TITO_ACTIVITY_II
+ */
+public class TITO_ACTIVITY_II {
+
+    public static void main(String[] args) {
+        System.out.println("Lloyd Tito");
+        System.out.println(19);
+        System.out.println(18);
+        System.out.println(19-18);
+       }
+}
