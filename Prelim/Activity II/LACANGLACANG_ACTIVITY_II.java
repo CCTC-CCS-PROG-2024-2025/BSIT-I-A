8803279724e952321c7059b6641b6e16b890913b
RyanStepGenz <ryanjune111@gmail.com>
2024-10-28 11:16:18 +0800

    Add files via upload

---
+++ Prelim/Activity II/LACANGLACANG_ACTIVITY_II.java
@@ -0,0 +1,12 @@
+/**
+ * LACANGLACANG_ACTIVITY_II
+ */
+public class LACANGLACANG_ACTIVITY_II {
+
+    public static void main(String[] args) {
+        System.out.println("Ryanjune LacangLacang");
+        System.out.println(18);
+        System.out.println(19);
+        System.out.println(18-19);
+       }
+}
