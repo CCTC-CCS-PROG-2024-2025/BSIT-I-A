a91e5e62bed297436b202fb73c77ee22807ca004
Jake <jakelargo99p@gmail.com>
2024-10-28 11:05:42 +0800

    Add files via upload

---
+++ Midterm/Activity III/LARGO_ACTIVITY_III.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class LARGO_ACTIVITY_III {
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the number");
+    int num = sc.nextInt();
+    while(num >= 0) {
+        System.out.println(num);
+        num=num-1;
+
+    }
+   }
+}//DDoS
