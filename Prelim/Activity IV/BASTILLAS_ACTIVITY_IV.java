f58347fd8908339974b9f316a66a466fff1c0740
Aj bastillas <archjunhelbastillas28@gmail.com>
2024-10-28 12:49:51 +0800

    Add files via upload

---
+++ Prelim/Activity IV/BASTILLAS_ACTIVITY_IV.java
@@ -0,0 +1,19 @@
+    public class BASTILLAS_ACTIVITY_IV {
+        public static void main(String[] args) {
+            int age1 = 6;
+            int age2 = 8;
+            int age3 = 10;
+            
+            int sum = age1 + age2 + age3;
+            int difference = age1 - age2 - age3;
+            int product = age1 * age2 * age3;
+            int qoutient = age1 / age2 / age3;
+
+            System.out.println(sum);
+            System.out.println(difference);
+            System.out.println(product);
+            System.out.println(qoutient);
+
+
+        }
+}       
