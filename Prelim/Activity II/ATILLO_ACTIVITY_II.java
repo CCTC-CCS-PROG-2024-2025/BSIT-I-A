5a7c8d74efbdb56b5c5f8b65fab4b1becd664f38
johnmico44567 <inuyashathestrongest@gmail.com>
2024-10-28 11:14:17 +0800

    Add files via upload

---
+++ Prelim/Activity II/ATILLO_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class ATILLO_ACTIVITY_II {
+    public static void main(String[] args) {   
+      System.out.println ("John Mico F. Atillo");
+      System.out.println(18);
+      System.out.println(17);  
+      System.out.println(18-17);  
+    }   
+}
