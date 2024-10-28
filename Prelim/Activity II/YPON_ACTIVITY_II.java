678435d1c69940aa113b1c7121e7fd65ab8f1f01
kylaaam <kylayu811@gmail.com>
2024-10-28 11:15:59 +0800

    Add files via upload

---
+++ Prelim/Activity II/YPON_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+public class YPON_ACTIVITY_II {
+    public static void main (String[] args) {
+System.out.println("Kyla Mae Ypon");
+System.out.println(18); 
+System.out.println(18);      
+System.out.println(18-18);
+}    
+  }     
