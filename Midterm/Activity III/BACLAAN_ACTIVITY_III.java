6feed370dc4e20b2c8ce0b5f6e76ea4b4d5db7d4
Harold Jel baclaan <baclaanharold401@gmail.com>
2024-10-28 11:05:19 +0800

    Add files via upload

---
+++ Midterm/Activity III/BACLAAN_ACTIVITY_III.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class BACLAAN_ASCTIVITY_III {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter Number");
+         int second = sc.nextInt();
+
+         while (second >= 0){
+            System.out.println(second);
+            second--;
+         }
+
+        }
+
+    }
