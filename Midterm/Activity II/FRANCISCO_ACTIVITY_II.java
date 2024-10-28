9d069a4b26c046a5f8f6a6e4258cadb2a8fade72
franciscomaricar <maricarfrancisco737@gmail.com>
2024-10-28 11:18:50 +0800

    Add files via upload

---
+++ Midterm/Activity II/FRANCISCO_ACTIVITY_II.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class FRANCISCO_ACTIVITY_II {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("input age");
+        int age = sc.nextInt();
+
+        if (age >= 0 && <= 11) {
+
+        }
+    }
+}
