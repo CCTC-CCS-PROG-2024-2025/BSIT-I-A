72c8de599e431db971b2a63058bf9b6a22aa9428
Just2inches <alvenjamesa@gmail.com>
2024-10-28 11:20:54 +0800

    Add files via upload

---
+++ Prelim/Activity II/ARGONILLO_ACTIVITY_II.java
@@ -0,0 +1,17 @@
+class ARGONCILLO_ACTIVITY_II{
+    public static void main(String[] args) {
+        System.out.println("Alven James Argoncillo");
+        System.out.println(18);
+        System.out.println(18);
+        System.out.println(18);
+
+        
+    }
+}
+
+
+
+
+
+
+
