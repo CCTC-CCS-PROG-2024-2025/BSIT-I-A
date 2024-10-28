827e6cb87614dd6e7a47dd419a4e23e048bf18a4
FidelVilla02 <villafidel96@gmail.com>
2024-10-28 11:14:54 +0800

    Add files via upload

---
+++ Prelim/Activity IV/VILLA_F_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class VILLA_F_ACTIVITY_IV {
+    public static void main(String[] args) {
+      int a = 10;
+      int b = 20;
+      int c = 30;
+
+      int sum = a+b+c;
+      int sub = a-b-c;
+      int mult = a*b*c;
+      int div = a/b/c;
+
+      System.out.println(sum);
+      System.out.println(sub);
+      System.out.println(mult);
+      System.out.println(div);
+    }
+}
