1c8cf0e5c8f8ea0134cdbddf3bc4b13dd12e8751
Nathaniel-Lao <nathaniellao2005@gmail.com>
2024-10-28 11:00:35 +0800

    Add files via upload

---
+++ Midterm/Activity III/LAO_ACTIVITY_III.java
@@ -0,0 +1,13 @@
+public class LAO_ACTIVITY_III {
+    
+    public static void main(String[] args) {
+        int x = 1000;
+
+        while (x >=0) {
+        System.out.println(x);
+        x --;
+     }
+  }
+
+
+}
