fb99abad3070aa0e901e2eb7e5ba1be59cc6cbda
RageDRX <drixgael@gmail.com>
2024-10-28 11:05:49 +0800

    Add files via upload

---
+++ Prelim/Activity II/ARCAY_ACTIVITY_II.java
@@ -0,0 +1,18 @@
+class ARCAY_ACTIVITY_II{
+    public static void main (String[] args) {
+        System.out.println("Noely Jed E. Arcay");
+        System.out.println(17);
+        System.out.println(18);
+        System.out.println(17-18);
+    }
+}
+
+
+
+
+
+
+
+
+
+ 
