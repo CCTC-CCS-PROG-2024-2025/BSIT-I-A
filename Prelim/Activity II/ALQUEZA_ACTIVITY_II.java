68335c09053390f929b9de4aac236d43e6a3b633
Zendich <sendic.alqueaza08@gmail.com>
2024-10-28 11:17:10 +0800

    Add files via upload

---
+++ Prelim/Activity II/ALQUEZA_ACTIVITY_II.java
@@ -0,0 +1,11 @@
+/**
+ * ALQUEZA_ACTIVITY_II
+ */
+public class ALQUEZA_ACTIVITY_II {
+    public static void main(String[] args) {
+        System.out.println("Alqeaza Sendic Jacob M.");
+        System.out.println(19);
+        System.out.println(19);
+        System.out.println(19-19);
+    }
+}
