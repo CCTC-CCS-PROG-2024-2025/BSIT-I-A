5fa0b2f2854c0b01b69fcea537ca7fdbc0669feb
nhoviecamingao20 <nhoviecamingao25@gmail.com>
2024-10-28 11:16:22 +0800

    Add files via upload

---
+++ Prelim/Activity II/CAMINGAO_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class CAMINGAO_ACTIVITY_II {
+    public static void main (String[] args) {
+        System.out.println("Nhovie Camingao");
+        System.out.println(18);
+        System.out.println(18);
+        System.out.println(18-18);
+    }
+}
