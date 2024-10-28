4f5957b5eb24fd7330178a1f775d5962eefbae4f
ailene-vi <ailenevilla3@gmail.com>
2024-10-28 10:57:02 +0800

    Add files via upload

---
+++ Midterm/Activity III/VILLA_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class VILLA_ACTIVITY_III {
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Input timer: ");
+        int x = sc.nextInt();
+
+        do{
+            System.out.println(x);
+            x++;
+        }   while(x <= 5);
+        }
+    }
+    
+
