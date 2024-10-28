840cec94008f82267976685198adb96c1071dcb9
Calvin John Panla-an <166713172+Calvon33123@users.noreply.github.com>
2024-10-28 10:56:25 +0800

    Add files via upload

---
+++ Midterm/Activity III/PANLAAN_MIDTERM_ACTIVITY_III.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class PANLAAN_MIDTERM_ACTIVITY_III {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Input Second Number: ");
+            int sec = sc.nextInt();
+
+            while(sec>=0){
+            System.out.println(sec);
+            sec--;
+        }
+    }
+ }
