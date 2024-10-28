19b90dcd78f9b2783ce61fdaf576553860c214a5
ailene-vi <ailenevilla3@gmail.com>
2024-10-28 12:16:58 +0800

    Add files via upload

---
+++ Prelim/Activity IV/VILLA_A_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class VILLA_A_ACTIVITY_IV {
+    public static void main(String[] args){
+        int a = 6;
+        int b = 5;
+        int c = 3;
+
+        int sum = (a+b);
+        int diff = (b-c);
+        int quot = (c/a);
+        int ans = (a*b*c);
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(quot);
+        System.out.println(ans);
+    }
+}
