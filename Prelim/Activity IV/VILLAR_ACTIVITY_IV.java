fab6e754b363ed56d4f65129d048f3de282b1ff1
nommuu <167503476+nommuu@users.noreply.github.com>
2024-10-28 11:07:54 +0800

    Add files via upload

---
+++ Prelim/Activity IV/VILLAR_ACTIVITY_IV.java
@@ -0,0 +1,25 @@
+public class VILLAR_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 50;
+        int c = 150; 
+
+        int sum = a+b+c;
+        int product = a*b*c;
+        int difference = a-b-c;
+        int quotient = a/b/c;
+        int total = sum+product+difference+quotient;
+
+        System.out.println("  R   E   S   U   L   T   S   ");
+        System.out.println();
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+        System.out.println("Product = " + product);
+        System.out.println("Quotient = " + quotient);
+        System.out.println();
+        System.out.println("Total = " + total);
+        System.out.println();
+        // ohaha
+    }
+    
+}
