2b3104edf23745d22ab0a0776607d38a22d4f70e
Jake <jakelargo99p@gmail.com>
2024-10-28 11:16:25 +0800

    Add files via upload

---
+++ Prelim/Activity II/LARGO_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class LARGO_ACTIVITY_II {
+    public static void main (String[] args) {
+        System.out.println("JAKE L LARGO");
+        System.out.println(18);
+        System.out.println(18);
+        System.out.println(18-18);
+    }
+}
