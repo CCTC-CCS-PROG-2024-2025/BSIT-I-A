2566cbad54747e3c1d59394731618fa65d810a12
Jumar Dizon <swordego2019@gmail.com>
2024-10-28 11:15:42 +0800

    Add files via upload

---
+++ Prelim/Activity II/DIZON_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class DIZON_ACTIVITY_II {
+    public static void main (String[] args) {
+        System.out.println("Dizon, Jumar S.");
+        System.out.println(27);
+        System.out.println(18);
+        System.out.println(27-18);
+    }
+}
