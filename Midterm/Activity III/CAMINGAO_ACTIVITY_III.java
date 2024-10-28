8e64b2c81d4fd8f2c14b0948433a27a3cea4cbc2
nhoviecamingao20 <nhoviecamingao25@gmail.com>
2024-10-28 11:05:19 +0800

    Add files via upload

---
+++ Midterm/Activity III/CAMINGAO_ACTIVITY_III.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class CAMINGAO_ACTIVITY_III {
+    public static void main (String []args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter Timer: ");
+        int timer = sc.nextInt(); 
+    
+        while (timer >=0) {
+            System.out.println(timer);
+            timer --;
+        }
+        }
+} 
