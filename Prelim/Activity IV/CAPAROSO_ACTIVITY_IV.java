bc60583256f18cc0b2c4e0a44ead9a3507f42aa5
Nilfa-03 <caparosonilfa@gmail.com>
2024-10-28 11:19:32 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CAPAROSO_ACTIVITY_IV.java
@@ -0,0 +1,20 @@
+class CAPAROSO_ACTIVITY_IV{
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = a+b;
+
+        int sum=(a+b);
+        int total=(c-a);
+        int result=(a*b);
+        int res=(c/a);
+
+        System.out.println(sum);
+        System.out.println(total);
+        System.out.println(result);
+        System.out.println(res);
+
+
+    }
+}
