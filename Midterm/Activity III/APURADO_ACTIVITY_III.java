72841f8f5725079e18b35c380dbf620707e2a68f
apuradoxjefer <jeferapurado6@gmail.com>
2024-10-28 11:03:54 +0800

    Add files via upload

---
+++ Midterm/Activity III/APURADO_ACTIVITY_III.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class APURADO_ACTIVITY_III {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input Number: ");
+        int second = sc.nextInt();
+
+        while(second>=0){
+            System.out.println(second);
+            second--;
+            
+        }
+    }
+}   
+
