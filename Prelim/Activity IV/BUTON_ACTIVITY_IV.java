7f201846213c81473337db71cad0cf108cac6720
jeanmaica1810 <jeanbuton754@gmail.com>
2024-10-28 12:53:09 +0800

    Add files via upload

---
+++ Prelim/Activity IV/BUTON_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class BUTON_ACTIVITY_IV {
+    public static void main (String[] args) {
+        int a = 5;
+        int f = 3;
+        int d = 2;
+
+        int sum = a + f + d;
+        int diff = a - f - d;
+        int prod = a * f * d;
+        int quot = a / f / d;
+        
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+    }              
+}
