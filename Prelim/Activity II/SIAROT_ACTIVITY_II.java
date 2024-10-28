f6b263ed0f3b7756e77398132e33ef301414ebbc
kazcel2 <siarotkazcel23@gmail.com>
2024-10-28 11:27:08 +0800

    Add files via upload

---
+++ Prelim/Activity II/SIAROT_ACTIVITY_II.java
@@ -0,0 +1,10 @@
+class SIAROT_ACTIVITY_II {
+    public static void main(String[] args) {
+        System.out.println("Kazcel Siarot");
+        System.out.println(20);
+        System.out.println(20);
+        System.out.println(20-20);
+    }
+}    
+    
+
