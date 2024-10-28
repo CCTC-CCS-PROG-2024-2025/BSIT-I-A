73503ac4d3ce6e3e7e18e5b9c97db958a806a609
Aj bastillas <archjunhelbastillas28@gmail.com>
2024-10-28 12:49:06 +0800

    Add files via upload

---
+++ Prelim/Activity II/BASTILLAS_ACTIVITY_II.java
@@ -0,0 +1,12 @@
+ public class BASTILLAS_ACTIVITY_II {
+    public static void main (String[]args) {
+         System.out.println("AJ BASTILLAS");
+         System.out.println(18);
+         System.out.println(18);
+         System.out.println(18 - 18);
+    
+    }         
+
+}  
+
+     
