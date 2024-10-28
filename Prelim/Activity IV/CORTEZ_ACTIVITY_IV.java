53670b996a6b2a13f91abcc4652f090f3d84872a
John Ford <lustegwaps@gmail.com>
2024-10-28 11:05:42 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CORTEZ_ACTIVITY_IV.java
@@ -0,0 +1,20 @@
+class CORTEZ_ACTIVIY_IV{
+  public static void main(String{} args) {
+    int age1 = 19;
+    int age2 = 26;
+    int age3 = 13;
+
+    int sum = age1+age2+age3+;
+    int result = age1/age2/age3;
+    int mult = age1*age2*age3*;
+    int diff = age1-age2-age3;
+    
+    system.out.println(sum);
+    system.put.println(result);
+    system.put.println(mult);
+    system.out.println(diff);
+  }
+}
+    
+     
+  
