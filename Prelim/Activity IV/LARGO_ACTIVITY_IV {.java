4ce12a82b7674a97b22b76d0ef8925c495cbba6c
Jake <jakelargo99p@gmail.com>
2024-10-28 11:17:23 +0800

    Add files via upload

---
+++ Prelim/Activity IV/LARGO_ACTIVITY_IV {.java
@@ -0,0 +1,11 @@
+class LARGO_ACTIVITY_IV {
+    public static void main (String[] args) {
+
+        int coke = 3;
+        int sprite = 6;
+        int result = 3+6;
+        
+        system.out.println(result);
+    }
+}
+        
