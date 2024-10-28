782f7a7627c054260b863afbd4de573db27d08de
pookie-bear11 <ethangabrieldelapena@gmail.com>
2024-10-28 10:55:48 +0800

    Add files via upload

---
+++ Midterm/Activity III/DELAPEÑA_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class DELAPEÑA_ACTIVITY_III {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Seconds: ");
+    int Seconds = sc.nextInt();
+
+    while(Seconds>=0){
+      System.out.println(Seconds);
+      Seconds--;
+    }
+  }
+  
+}
