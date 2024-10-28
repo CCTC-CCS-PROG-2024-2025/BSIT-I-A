4f0db1ca05bc5cbd0559e444043e90d3a92a7675
pookie-bear11 <ethangabrieldelapena@gmail.com>
2024-10-28 11:06:17 +0800

    Add files via upload

---
+++ Prelim/Activity II/DELAPEÑA_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class DELAPEÑA_ACTIVITY_II {
+  public static void main(String[] args) {
+    System.out.println("Ethan Gabriel M. Dela Pena");
+    System.out.println(18);
+    System.out.println(18);
+    System.out.println(18 - 18);
+  }
+}
