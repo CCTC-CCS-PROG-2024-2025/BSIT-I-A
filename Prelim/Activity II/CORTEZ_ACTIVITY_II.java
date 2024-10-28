f9c69c484603d8af99fb4fcedced856d5e82b877
John Ford <lustegwaps@gmail.com>
2024-10-28 11:05:05 +0800

    Add files via upload

---
+++ Prelim/Activity II/CORTEZ_ACTIVITY_II.java
@@ -0,0 +1,7 @@
+class CORTEZ_ACTIVITY_II
+public static void main(String[] args){
+    System.out.println("John Ford P Cortez");
+    System.out.println(18);
+    System.out.println(19);
+    System.out.println(18-19);
+} 
