62232b4805748a4730bb7a2ecf5fdbd5615b1a43
Jumar Dizon <swordego2019@gmail.com>
2024-10-28 11:05:24 +0800

    Add files via upload

---
+++ Midterm/Activity III/DIZON_ACTIVITY_ III.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class DIZON_ACTIVITY_III {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int x = sc.nextInt();
+       
+        int y = x;
+
+        do {
+            System.out.println(y);
+            y--;
+        } while (y >=1);
+        
+        sc.close();
+    }
+}        
