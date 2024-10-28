ac57a231790d65f83eba9914cf39aef481fa1aef
shofeey <totskee12345@gmail.com>
2024-10-28 11:25:03 +0800

    Add files via upload

---
+++ Midterm/Activity III/RAGODOS_ACTIVITY_III.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class RAGODOS_ACTIVITY_III {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER NUMBER:");
+        int x = sc.nextInt();
+        while(x > -1){
+            System.out.println(x);
+            x = x-1;
+        }
+    }
+}
