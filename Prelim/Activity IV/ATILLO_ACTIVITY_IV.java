2a2b6c9a52f8a42f4ae08a5c6e6cfc7cec094da7
johnmico44567 <inuyashathestrongest@gmail.com>
2024-10-28 11:16:54 +0800

    Add files via upload

---
+++ Prelim/Activity IV/ATILLO_ACTIVITY_IV.java
@@ -0,0 +1,20 @@
+class ATILLO_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int age1 = 15;
+        int age2 = 28;
+        int age3 = 11;
+        
+        int sum = +age1+age2;
+        int result = age1/age2/age3;
+        int mult = age1*age2*age3;
+        int diff = age1-age2-age3;
+    
+        System.out.println(sum);
+        System.out.println(result);
+        System.out.println(mult);
+        System.out.println(diff);
+        
+        
+    }
+    
+    }
