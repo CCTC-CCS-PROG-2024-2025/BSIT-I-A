27c0a17d8ba8532c35de0982e141aef68ee7c4b0
jeancentmigallen <migallenjeancentmay@gmail.com>
2024-10-28 11:20:38 +0800

    Add files via upload

---
+++ MIGALLEN_ACTIVITY_III.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MIGALLEN_ACTIVITY_III {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int second = sc.nextInt();
+
+        while (second >-1){
+            System.out.println(second);
+            second--;
+        }
+    }
+}
+    
+}
