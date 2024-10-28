3c4f20f53a5bca02e48126b8ed377704f12d3dd5
FidelVilla02 <villafidel96@gmail.com>
2024-10-28 11:14:35 +0800

    Add files via upload

---
+++ Prelim/Activity II/VILLA_F_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class VILLA_F_ACTIVITY_II {
+    public static void main(String[] args) {
+        System.out.println("Fidel Villa");
+        System.out.println(21);
+        System.out.println(18);
+        System.out.println(21-18);
+    }
+}
