267b476bdf23199b9f65ead0f4d06fbdb1551fb6
nhoviecamingao20 <nhoviecamingao25@gmail.com>
2024-10-28 11:16:59 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CAMINGAO_ACTIVITY_lV.java
@@ -0,0 +1,17 @@
+class CAMINGAO_ACTIVITY_IV {
+    public static void main(String[] args) {   
+        int a = 7;
+        int b = 2;
+        int c = 3;
+        
+        int sum  = a + b + c;
+        int diff = a - b - c;
+        int prod = a * b * c;
+        int quot = a / b / c;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+    }
+}
