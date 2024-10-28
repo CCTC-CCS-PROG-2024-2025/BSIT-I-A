3f7df8f071604ad627a457dfca1b0f02e2e9942f
pookie-bear11 <ethangabrieldelapena@gmail.com>
2024-10-28 11:06:48 +0800

    Add files via upload

---
+++ Prelim/Activity IV/DELAPEÑA_ACTIVITY_IV.java
@@ -0,0 +1,18 @@
+public class DELAPEÑA_ACTIVITY_IV {
+  public static void main(String[] args) {
+    int a = 50;
+    int b = 50;
+    int c = 50;
+
+    int sum = a + b + c;
+    int difference = a - b - c;
+    int product = a * b * c;
+    int quotient = a / b / c;
+
+    System.out.println(sum);
+    System.out.println(difference);
+    System.out.println(product);
+    System.out.println(quotient);
+  }
+
+}
