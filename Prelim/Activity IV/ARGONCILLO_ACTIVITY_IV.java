50600e275a074992e1618ec378bbe98bec76dd50
Just2inches <alvenjamesa@gmail.com>
2024-10-28 11:22:40 +0800

    Add files via upload

---
+++ Prelim/Activity IV/ARGONCILLO_ACTIVITY_IV.java
@@ -0,0 +1,23 @@
+class ARGONCILLO_ACTIVITY_IV {
+    public static void main (String[] args) {
+        int a = 18;
+        int b = 27;
+        int c = 17;
+        
+        int sum = (a + b);
+        int difference = (a - b);
+        int product = (a * b);
+        int qoutient = (a / b);
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(qoutient);
+
+    }
+}    
+
+
+
+
+
