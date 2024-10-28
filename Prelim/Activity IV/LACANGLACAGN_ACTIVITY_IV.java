8f1a314520a3a612beb4604a836dcdce1bf310e2
RyanStepGenz <ryanjune111@gmail.com>
2024-10-28 11:16:36 +0800

    Add files via upload

---
+++ Prelim/Activity IV/LACANGLACAGN_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+public class LACANGLACAGN_ACTIVITY_IV {
+    public class LACANGLACANG_IV {
+        public static void main(String[] args) {
+            int a = 2;
+            int b = 5;
+            int c = 8;
+            int sum = a+b+c;
+            int difference = c-b-a;
+            int product = a*b*c;
+            int quotient = c/a/b;
+            System.out.println(sum);
+            System.out.println(difference);
+            System.out.println(product);
+            System.out.println(quotient);
+        }
+    }
+}
