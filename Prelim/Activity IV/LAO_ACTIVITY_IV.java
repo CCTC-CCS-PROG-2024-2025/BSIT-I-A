1b10cd301bb8c39dbf6690bdf0b91f0f319c6dca
Nathaniel-Lao <nathaniellao2005@gmail.com>
2024-10-28 11:07:47 +0800

    Add files via upload

---
+++ Prelim/Activity IV/LAO_ACTIVITY_IV.java
@@ -0,0 +1,10 @@
+class Lao_ACTIVITY_IV {
+    public static void main(String[] args) {
+        double price1 = 23;
+        double price2 = 23;
+        
+        double sum = price1 + price2;
+
+        System.out.println(sum);
+    }  
+}
