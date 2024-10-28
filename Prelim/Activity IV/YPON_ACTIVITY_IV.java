c0826803bb39fb40697223a0f04e3109a152a3e0
kylaaam <kylayu811@gmail.com>
2024-10-28 11:16:24 +0800

    Add files via upload

---
+++ Prelim/Activity IV/YPON_ACTIVITY_IV.java
@@ -0,0 +1,19 @@
+
+public class YPON_ACTIVITY_IV {
+public static void main(String[] args) {
+    int a = 1;
+    int b = 2;
+    int c = 3;
+
+    int sum = a + b + c;
+    int diff = a - b - c;
+    int prod = a * b * c;
+    int quot = a / b / c;
+
+    System.out.println(sum);
+    System.out.println(diff);
+    System.out.println(prod);
+    System.out.println(quot);
+ }
+}
+
