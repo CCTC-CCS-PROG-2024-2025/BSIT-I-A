acd7a2a2072000e2611949ab5863b3399dc73361
Nilfa-03 <caparosonilfa@gmail.com>
2024-10-28 11:19:05 +0800

    Add files via upload

---
+++ Prelim/Activity II/CAPAROSO_ACTIVITY_II.java
@@ -0,0 +1,9 @@
+public class CAPAROSO_ACTIVITY_II {
+    public static void main(String[] args) {
+        System.out.println("Nilfa Mica Caparoso");
+        System.out.println("20");
+        System.out.println("20");
+        System.out.println("20+20");
+
+    }
+}
