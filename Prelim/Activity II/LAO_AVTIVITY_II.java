e10fd1cdb5aea110c7c3a681cfe24f2787ee7b7d
Nathaniel-Lao <nathaniellao2005@gmail.com>
2024-10-28 11:06:36 +0800

    Add files via upload

---
+++ Prelim/Activity II/LAO_AVTIVITY_II.java
@@ -0,0 +1,7 @@
+class nathaniel {}
+     public static void main(String[] arg) {
+        System.out.println( "Nathaniel Alexis Lao" );
+        System.out.println(18);
+        System.out.println(18);
+        System.out.println(18-18);
+     }    
