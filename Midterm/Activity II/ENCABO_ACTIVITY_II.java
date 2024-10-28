2d13bedb44d801fa68e078723acac3feeb1732a2
nenakassandraencabo <nenakassandraencabo@gmail.com>
2024-10-28 11:23:44 +0800

    Add files via upload

---
+++ Midterm/Activity II/ENCABO_ACTIVITY_II.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class ENCABO_ACTIVITY_II {
+    public static void main ( String args ) { 
+        System.out.println(x:"Enter the age");
+        int age = jm.nextInt();
+
+        if (age >= 0 && age <= 11) {
+             System.out.println(x:"Child");
+
+        else if (age >= 12 && age <=17);{
+            System.out.println(x:"Teen");
+
+        else if (age >= 18 && age <= 64) {
+            System.out.println(x:"Adult");
+     }
+    
+    }
+   }
