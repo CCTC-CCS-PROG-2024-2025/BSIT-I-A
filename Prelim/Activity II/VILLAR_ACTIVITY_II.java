18737115a2c8860ec75ee2caf85bf81a083be75b
nommuu <167503476+nommuu@users.noreply.github.com>
2024-10-28 11:07:39 +0800

    Add files via upload

---
+++ Prelim/Activity II/VILLAR_ACTIVITY_II.java
@@ -0,0 +1,9 @@
+class VILLAR_ACTIVITY_II {
+    public static void main(String[] args) {
+        System.out.println("Edison Villar");
+        System.out.println(18);
+        System.out.println(18);
+        System.out.println(18 - 18);
+
+    }
+}
