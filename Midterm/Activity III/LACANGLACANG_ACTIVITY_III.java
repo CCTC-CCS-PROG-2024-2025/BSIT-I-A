bb065c1e9c9d6c87a8645097cf212b0017d3b189
RyanStepGenz <ryanjune111@gmail.com>
2024-10-28 11:14:57 +0800

    Add files via upload

---
+++ Midterm/Activity III/LACANGLACANG_ACTIVITY_III.java
@@ -0,0 +1,14 @@
+
+    import java.util.Scanner;
+    
+    public class LACANGLACANG_ACTIVITY_III {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter Seconds Number: ");
+            int sec = sc.nextInt();
+            while (sec >= 0) {
+                System.out.println(sec);
+                sec--;
+            }
+        }
+    }
