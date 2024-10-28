1f067b92c96b00001f293273169cefa1f061b9c0
L3BR0NJ4M3S <lebronjamesabong@gmail.com>
2024-10-28 10:52:45 +0800

    Add files via upload

---
+++ Midterm/Activity III/ABONG_ACTIVITY_PROG_III.java
@@ -0,0 +1,14 @@
+ class ABONG_ACTIVITY_PROG_III {
+    
+    public static void main(String[] arg) {
+        int x = 5;
+
+        while(x >=0) {
+            System.out.println(x);
+            x--;
+        }
+        
+    }
+
+
+}
