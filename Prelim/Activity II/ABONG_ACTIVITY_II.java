376ca64e12b24689ed3a194b673b09144440a952
L3BR0NJ4M3S <lebronjamesabong@gmail.com>
2024-10-28 11:07:41 +0800

    Add files via upload

---
+++ Prelim/Activity II/ABONG_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class lebron {
+     public static void main (String [] arg) { 
+        System.out.println("Lebron James Abong ");
+        System.out.println(18);
+        System.out.println(18); 
+        System.out.println(18-18);
+    }
+}
