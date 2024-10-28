69083e842b1fe9caf87ac8c186cd40ba0ebbc7d7
Lloyd Tito <notlloydtito@gmail.com>
2024-10-28 11:19:07 +0800

    Add files via upload

---
+++ Prelim/Activity IV/TITO_ACTIVITY_IV.java
@@ -0,0 +1,15 @@
+public class TITO_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        int c = 8;
+        int sum = a+b+c;
+        int difference = c-b-a;
+        int product = a*b*c;
+        int quotient = c/a/b;
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+    }
+}
