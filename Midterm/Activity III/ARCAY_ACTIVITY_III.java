687b64f34d246b1dfde6b35a506b16c31cc8f82e
RageDRX <drixgael@gmail.com>
2024-10-28 10:56:17 +0800

    Add files via upload

---
+++ Midterm/Activity III/ARCAY_ACTIVITY_III.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class ARCAY_ACTIVITY_III {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input the second: ");
+        int second = sc.nextInt();
+
+        while(second>=0){
+            System.out.println(second);
+            second--;
+        }
+    }
+}
