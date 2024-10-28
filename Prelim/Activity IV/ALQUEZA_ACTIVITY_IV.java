250bfe6b07e528ae650a1b6c81928d40270d8361
Zendich <sendic.alqueaza08@gmail.com>
2024-10-28 11:17:41 +0800

    Add files via upload

---
+++ Prelim/Activity IV/ALQUEZA_ACTIVITY_IV.java
@@ -0,0 +1,16 @@
+public class ALQUEZA_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int A = 20;
+        int B = 30;
+        int C = 2;
+        int sum = A + B + C;
+        int difference = A - B - C;
+        int product = A*B*C;
+        int quotient = A/B/C;
+        System.out.println(sum);
+        System.out.println(product);
+        System.out.println(difference);
+        System.out.println(quotient);
+
+    }
+}
