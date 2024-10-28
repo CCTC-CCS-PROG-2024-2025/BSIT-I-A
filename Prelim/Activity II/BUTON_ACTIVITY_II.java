e17c373865a24fb202c52f8da09a2087453c96ba
jeanmaica1810 <jeanbuton754@gmail.com>
2024-10-28 12:51:47 +0800

    Add files via upload

---
+++ Prelim/Activity II/BUTON_ACTIVITY_II.java
@@ -0,0 +1,13 @@
+public class BUTON_ACTIVITY_II {
+    public static void main (String[] args) {
+       String name = "Jean"; 
+       int age = 18;
+       float height = 167.4f;
+       double weight = 89.5;
+
+        System.out.println ("name");
+        System.out.println (age);
+        System.out.println (height);
+        System.out.println (weight); 
+    }
+}
