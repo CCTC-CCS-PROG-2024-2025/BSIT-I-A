4f59254b44f23727bd8073b8151b07bba4be0990
apuradoxjefer <jeferapurado6@gmail.com>
2024-10-28 12:15:43 +0800

    Add files via upload

---
+++ Prelim/Activity II/APURADO_ACTIVITY_II.java
@@ -0,0 +1,9 @@
+class APURADO_ACTIVITY_II{
+    public statis void main(String[] args){
+        System.out.println("Jefer A. Apurado");
+        System.out.println("19");
+        System.out.println("19");
+        System.out.println(19 + 19);
+    }
+} 
+
