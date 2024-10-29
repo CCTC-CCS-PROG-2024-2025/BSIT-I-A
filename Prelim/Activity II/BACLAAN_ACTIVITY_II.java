c185267a6c76c68583d9372aabdac5f044c7b469
Harold Jel baclaan <baclaanharold401@gmail.com>
2024-10-29 17:38:50 +0800

    Add files via upload

---
+++ Prelim/Activity II/BACLAAN_ACTIVITY_II.java
@@ -0,0 +1,9 @@
+class BACLAAN_ACTIVITY_II{
+    public static void main(String[]args){
+        System.out.println("Perceval Cabaña");
+            System.out.println(19);
+            System.out.println(19);
+            System.out.println(19+19);
+    }
+    
+    }
