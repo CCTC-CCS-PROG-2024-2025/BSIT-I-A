af003580c89ed8cf4f6b889385fda6e19002b1fd
nenakassandraencabo <nenakassandraencabo@gmail.com>
2024-10-29 15:48:31 +0800

    Delete Prelim/Activity II/ENCABO_ACTIVITY_II.java

--- Prelim/Activity II/ENCABO_ACTIVITY_II.java
+++
@@ -1,9 +0,0 @@
-class ENCABO_ACTIVITY_II{
-    public static void main(String[]args){
-        System.out.println("Princess Ericka L. Jungoy");
-            System.out.println(19);
-            System.out.println(20);
-            System.out.println(19+20);
-    }
-    
-    }
