20ef167ebd2be5c4022cfee0c52629238e18b2fb
Calvin John Panla-an <166713172+Calvon33123@users.noreply.github.com>
2024-10-29 23:42:53 +0800

    Add files via upload

---
+++ Prelim/Activity IV/PANLAAN_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class PANLAAN_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int age1 = 18;
+        int age2 = 32;
+        int age3 = 16;
+        
+        int sum = age1+age2+age3;
+        int res = age1/age2/age3;
+        int mul = age1*age2*age3;
+        int dif = age1-age2-age3;
+
+        System.out.println(sum);
+        System.out.println(res);
+        System.out.println(mul);
+        System.out.println(dif);
+    }
+}
