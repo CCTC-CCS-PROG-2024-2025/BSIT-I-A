1ff827d5c9ddef4361d6ad438259e3885d623975
Harold Jel baclaan <baclaanharold401@gmail.com>
2024-10-29 17:42:34 +0800

    Add files via upload

---
+++ Prelim/Activity VI/BACLAAN_ACTIVITY_VI.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ 
+class BACLAAN_ACTIVITY_VI{
+   public static void main(String[] args) {
+       Scanner sc = new Scanner (System.in);
+       System.out.print("Enter your height: ");
+       int height = sc.nextInt();
+       System.out.print("Enter your weight: ");
+       int weight = sc.nextInt();
+       int sum = (height+weight);
+       int diff = (height-weight);
+       int prod = (height/weight);
+       System.out.println("Sum: "+sum);
+       System.out.println("Diff: "+diff);
+       System.out.println("Prod: "+prod);
+   }
+}
