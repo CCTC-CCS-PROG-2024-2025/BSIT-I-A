20be90bd1de817723e882f1455b5523c8e95ce47
dzxhsy <bacalladaisyree4@gmail.com>
2024-10-29 18:51:18 +0800

    Add files via upload

---
+++ Prelim/Activity II/BACALLA_ACTIVITY_II.java
@@ -0,0 +1,9 @@
+class BACALLA_ACTIVITY_II{
+    public static void main(String[]args){
+        System.out.println("Maricar Francisco");
+            System.out.println(19);
+            System.out.println(19);
+            System.out.println(19+19);
+    }
+    
+    }
