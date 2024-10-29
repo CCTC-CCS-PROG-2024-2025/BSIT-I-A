013195b48de96d15a6dc0a7079767b3ead85abe0
Perceval00 <percevalcabana04@gmail.com>
2024-10-29 15:59:28 +0800

    Add files via upload

---
+++ Prelim/Activity II/CABANA_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class CABANA_ACTIVITY_II{
+    public static void main(String[] args){
+        System.out.println("Perceval Cabana");
+        System.out.println(19);
+        System.out.println(19);
+        System.out.println(19 - 19);
+    }
+}
