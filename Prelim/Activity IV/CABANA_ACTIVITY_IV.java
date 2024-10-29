b4dd4e4d39ae90c629095231839877688b19bd25
Perceval00 <percevalcabana04@gmail.com>
2024-10-29 16:00:01 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CABANA_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+ class CABANA_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 9;
+        int c = 2;
+
+        int sum = (a+b);
+        int diff = (b-c);
+        int quo = (c/a);
+        int prod = (a*b*c);
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(quo);
+        System.out.println(prod);
+    }
+}
