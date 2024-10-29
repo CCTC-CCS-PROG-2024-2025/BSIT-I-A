1ba0692ad82561828fc29a1f7d6ee14625e4472a
franciscomaricar <maricarfrancisco737@gmail.com>
2024-10-29 15:08:03 +0800

    Add files via upload

---
+++ Prelim/Activity IV/FRANCISCO_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+public class FRANCISCO_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 2;
+        int c = 3;
+
+        int sum = (a+b);
+        int diff = (b-c);
+        int quot = (c/a);
+        int ans = (a*b*c);
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(quot);
+        System.out.println(ans);
+    }
+}
