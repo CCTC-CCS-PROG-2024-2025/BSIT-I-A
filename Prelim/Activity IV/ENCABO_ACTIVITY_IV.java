fb8edf12bb39872c890e9dcb95453b5a24acf32b
nenakassandraencabo <nenakassandraencabo@gmail.com>
2024-10-29 14:47:28 +0800

    Add files via upload

---
+++ Prelim/Activity IV/ENCABO_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+public class ENCABO_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 3;
+        int c = 6;
+
+        int sum = (a+b);
+        int diff = (b-c);
+        int quot = (c/a);
+        int ans = (a*b*c);
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(quot);
+        System.out.println(ans);
+    }
+}
