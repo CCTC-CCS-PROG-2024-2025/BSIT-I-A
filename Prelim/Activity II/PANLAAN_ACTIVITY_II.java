7c42d1fbf8790c001a39a2ea9658831f53db5aaa
Calvin John Panla-an <166713172+Calvon33123@users.noreply.github.com>
2024-10-29 23:39:11 +0800

    Add files via upload

---
+++ Prelim/Activity II/PANLAAN_ACTIVITY_II.java
@@ -0,0 +1,18 @@
+class PANLAAN_ACTIVITY_II{
+    public static void main (String[] args) {
+        System.out.println("Calvin John C. Panla-an");
+        System.out.println(18);
+        System.out.println(18);
+        System.out.println(18-18);
+    }
+}
+
+
+
+
+
+
+
+
+
+ 
