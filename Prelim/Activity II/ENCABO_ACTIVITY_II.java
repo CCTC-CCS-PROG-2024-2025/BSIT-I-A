1c75c6642b2bea07379983c1c416b5a3f023c5ce
nenakassandraencabo <nenakassandraencabo@gmail.com>
2024-10-29 16:04:40 +0800

    Add files via upload

---
+++ Prelim/Activity II/ENCABO_ACTIVITY_II.java
@@ -0,0 +1,9 @@
+class ENCABO_ACTIVITY_II{
+    public static void main(String[]args){
+        System.out.println("Nena Kassandra S.Encabo");
+            System.out.println(18);
+            System.out.println(19);
+            System.out.println(18+19);
+    }
+    
+    }
