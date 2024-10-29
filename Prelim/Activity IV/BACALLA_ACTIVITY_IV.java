2475c5ba8cbc2fc21d7f587fe53d0004d64a26db
dzxhsy <bacalladaisyree4@gmail.com>
2024-10-29 18:53:34 +0800

    Add files via upload

---
+++ Prelim/Activity IV/BACALLA_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+public class BACALLA_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 9;
+        int c = 4;
+
+        int sum = (a+b);
+        int diff = (b-c);
+        int quot = (c/a);
+        int ans = (a*b*c);
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(quot);
+        System.out.println(ans);
+    }
+}
