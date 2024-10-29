707f87e4d1d72d43c99fec0b651638b64553934e
jeancentmigallen <migallenjeancentmay@gmail.com>
2024-10-29 16:00:09 +0800

    Add files via upload

---
+++ Prelim/Activity IV/MIGALLEN_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+public class MIGALLEN_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int a = 29;
+        int b = 15;
+        int c = 64;
+
+        int sum = (a+b);
+        int diff = (b-c);
+        int quot = (c/a);
+        int ans = (a*b*c);
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(quot);
+        System.out.println(ans);
+    }
+}
