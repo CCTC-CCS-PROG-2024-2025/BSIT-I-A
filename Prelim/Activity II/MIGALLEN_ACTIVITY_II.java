345fe07a31728cecd0aee7f3ed2e32c07628b0d3
jeancentmigallen <migallenjeancentmay@gmail.com>
2024-10-29 15:59:23 +0800

    Add files via upload

---
+++ Prelim/Activity II/MIGALLEN_ACTIVITY_II.java
@@ -0,0 +1,9 @@
+ class MIGALLEN_ACTIVITY_II{
+public static void main(String[]args){
+    System.out.println("Kent Louie P. Sumalo");
+        System.out.println(19);
+        System.out.println(20);
+        System.out.println(19+20);
+}
+
+}
