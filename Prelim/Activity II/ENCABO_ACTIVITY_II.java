6d26ffe7e01162514a0a154327cbc7674adba7c0
nenakassandraencabo <nenakassandraencabo@gmail.com>
2024-10-29 14:45:12 +0800

    Add files via upload

---
+++ Prelim/Activity II/ENCABO_ACTIVITY_II.java
@@ -0,0 +1,9 @@
+class ENCABO_ACTIVITY_II{
+    public static void main(String[]args){
+        System.out.println("Princess Ericka L. Jungoy");
+            System.out.println(19);
+            System.out.println(20);
+            System.out.println(19+20);
+    }
+    
+    }
