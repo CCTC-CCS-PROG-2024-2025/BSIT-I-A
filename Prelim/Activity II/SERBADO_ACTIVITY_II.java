721788a2cc5278f22eea2bdcd4b473154b8217ff
serbadop <serbadoprincessmaer@gmail.com>
2024-10-30 09:06:54 +0800

    Add files via upload

---
+++ Prelim/Activity II/SERBADO_ACTIVITY_II.java
@@ -0,0 +1,9 @@
+class SERBADO_ACTIVITY_II{
+    public static void main(String[]args){
+        System.out.println("Nena Encabo");
+            System.out.println(18);
+            System.out.println(18);
+            System.out.println(18+18);
+    }
+    
+    }
