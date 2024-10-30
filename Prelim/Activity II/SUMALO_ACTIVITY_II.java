0592792d10304745c4dc1b42bab9303d2fc82474
kentsumalo2004 <sumalokent37@gmail.com>
2024-10-30 20:39:35 +0800

    Add files via upload

---
+++ Prelim/Activity II/SUMALO_ACTIVITY_II.java
@@ -0,0 +1,9 @@
+class SUMALO_ACTIVITY_II{
+    public static void main(String[]args){
+        System.out.println("Jencent May Migallen");
+            System.out.println(19);
+            System.out.println(20);
+            System.out.println(19+20);
+    }
+    
+    }
