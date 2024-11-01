049746e03f9428ffb2f3994f8e53556b2794f288
James Ivan Panugaling <jamesivanipis@gmail.com>
2024-11-01 19:01:36 +0800

    Add files via upload

---
+++ Prelim/Activity IV/PANUGALING_ACTIVITY_IV.java
@@ -0,0 +1,20 @@
+public class PANUGALING_ACTIVITY_IV {
+    public static void main(String[] args) {
+        // Create 3 Variables
+        int age1 = 20;
+        int age2 = 10;
+        int age3 = 5;
+
+        // Performing simple equations
+        int sum = age1 + age2 + age3;
+        int difference = age1 - age2 - age3;
+        int product  = age1 * age2 * age3;
+        int quotient = age1 / age2 / age3;
+
+        // Printing Results
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+    } 
+}
