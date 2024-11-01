4b19e9ecd3c6ed0e4bc774dbfdb5501f6948eb20
James Ivan Panugaling <jamesivanipis@gmail.com>
2024-11-01 18:57:40 +0800

    Add files via upload

---
+++ Prelim/Activity II/PANUGALING_ACTIVITY_II.java
@@ -0,0 +1,10 @@
+public class PANUGALING_ACTIVITY_II {
+    public static void main(String[] args) {
+        System.out.println("James Ivan Panugaling");
+        System.out.println(18);
+        System.out.println(18);
+        System.out.println(18 - 18);
+         
+    }
+    
+}
