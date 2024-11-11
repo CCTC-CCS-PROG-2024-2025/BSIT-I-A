acabe07c685e2966b5391868401eaeb04da6c905
RyanStepGenz <ryanjune111@gmail.com>
2024-11-11 09:57:22 +0800

    Add files via upload

---
+++ Midterm/Activity IV/LACANGLACANG_ACTIVITY_IV.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class LACANGLACANG_ACTIVITY_IV {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Non-Negative Integer: ");
+        int num = sc.nextInt();
+        int i = 1, fact = 1;
+        while(i <= num) {
+            fact = fact * i;
+            i++;
+        }
+        System.out.println(fact);
+    }
+}
