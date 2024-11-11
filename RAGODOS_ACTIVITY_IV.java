012690303da3118c6b3f74c1750b54553828b8f9
shofeey <totskee12345@gmail.com>
2024-11-11 10:16:52 +0800

    Add files via upload

---
+++ RAGODOS_ACTIVITY_IV.java
@@ -0,0 +1,20 @@
+ public class RAGODOS_ACTIVITY_IV {
+    public static void main(String[] args) {
+        // create 3 Variables
+        int age1 = 20;
+        int age2 = 10;
+        int age3 = 2;
+        // Performing simple equations
+        int sum = age1 + age2 + age3;
+        int difference = age1 - age2 - age3;
+        int product = age1 * age2 * age3;
+        int quotient = age1 / age2 / age3;
+
+        //printing Results
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+        
+    }
+}
