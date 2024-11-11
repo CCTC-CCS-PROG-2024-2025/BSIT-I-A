bce8b975082cc9f1689ccb8f3b03c387ed5a531d
rjean-manyatay <patambagrjean@gmail.com>
2024-11-11 10:20:10 +0800

    Add files via upload

---
+++ Prelim/Activity VI/MANAYTAY_VI.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MANAYTAY_VI{
+    public static void main(String[] args){
+        @SuppressWarnings("resource")
+    Scanner sc= new Scanner(System.in);
+    System.out.print("Enter the number:");
+    int num1= sc.nextlnt();
+    System.out.print("Enterc the other number:");
+    int num2= sc.nextlnt();
+    int sum =num1 + num2;
+    int prod = num1 * num2;
+    int diff = num1 - num2;
+    System.out.println("Sum = " + sum);
+    System.out.println("Prod = " + prod);
+    System.out.println("Diff = " + diff);   
+
+    }
+}
