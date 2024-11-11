82c12cc5e9e1e549019bd7f7240e983de7c562ce
Lablab <laberneangco123@gmail.com>
2024-11-11 10:07:40 +0800

    Add files via upload

---
+++ Prelim/Activity IV/ANGCO_ACTIVITY_IV.java
@@ -0,0 +1,26 @@
+public class ANGCO_ACTIVITY_IV {
+        public static void main(String[] args) {
+
+         // Create 3 variabbles 
+
+         int age1 = 1;
+         int age2 = 2;
+         int age3 = 3;
+
+         // Performing single equations
+    
+         int sum = age1 + age2 + age3;
+         int difference = age1 - age2 - age3; 
+         int product = age1 * age2 * age3;
+         int quotient = age1 / age2 / age3;
+
+         // Printing results
+
+         System.out.println(sum);
+         System.out.println(difference);
+         System.out.println(product);
+         System.out.println(quotient);
+ 
+        }
+    }
+
