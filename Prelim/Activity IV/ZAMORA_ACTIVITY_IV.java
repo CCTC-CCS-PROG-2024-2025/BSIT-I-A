050d72f5ce09bc6e5f8da9e2c314ba250f9e0495
danielleandreizamora1 <kizukishimada@gmail.com>
2024-11-11 10:47:25 +0800

    Add files via upload

---
+++ Prelim/Activity IV/ZAMORA_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class ZAMORA_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int age1 = 18;
+        int age2 = 26;
+        int age3 = 12;
+        
+        int sum = age1+age2+age3;
+        int result = age1/age2/age3;
+        int mult = age1*age2*age3;
+        int diff = age1-age2-age3;
+
+        System.out.println(sum);
+        System.out.println(result);
+        System.out.println(mult);
+        System.out.println(diff);
+    }
+}
