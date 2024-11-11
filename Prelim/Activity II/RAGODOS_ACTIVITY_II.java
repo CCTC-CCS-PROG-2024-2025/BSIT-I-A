0b5ffa27f9eab513754c143da543db385b9f1ee1
shofeey <totskee12345@gmail.com>
2024-11-11 10:15:01 +0800

    Add files via upload

---
+++ Prelim/Activity II/RAGODOS_ACTIVITY_II.java
@@ -0,0 +1,12 @@
+public class RAGODOS_ACTIVITY_II {
+    public static void main(String [] arg) { 
+       System.out.println("Shofe ragodos ");
+       System.out.println(18);
+       System.out.println(18); 
+       System.out.println(18-18);
+
+
+
+       
+   }
+}
