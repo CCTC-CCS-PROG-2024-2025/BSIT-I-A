603b343159d4bb2c2e4ada3898254c418cdaed8f
danielleandreizamora1 <kizukishimada@gmail.com>
2024-11-11 10:47:08 +0800

    Add files via upload

---
+++ Prelim/Activity II/ZAMORA_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class ZAMORA_ACTIVITY_IV_ACTIVITY_II{
+    public static void main (String[] args) {
+        System.out.println("Danielle Andrei Zamora");
+        System.out.println(17);
+        System.out.println(18);
+        System.out.println(17-18);
+    }
+}
