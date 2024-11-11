ca36fdc9de1f29643fe4cf66bf9ab46d2f295834
nhoviecamingao20 <nhoviecamingao25@gmail.com>
2024-11-11 10:58:41 +0800

    Add files via upload

---
+++ Midterm/Activity IV/CAMINGAO_ACTIVITY_IV.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class CAMINGAO_ACTIVITY_IV {
+    public static void main(Sting[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a non-negative integer: ");
+        int num = scanner.nextInt();
+
+        long factorial =1;
+
+        for (int i = 1 i;<= num; i++ ) {
+            factorial *=1;
+        }
+
+        System.out.print("Factorial of " + num + "is: " + factorial);
+
+        scanner.close();
+    }
+}
+
+
+
+
