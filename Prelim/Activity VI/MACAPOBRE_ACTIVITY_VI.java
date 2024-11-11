3cf841984b306c91df951f94c363c3fb2ac004d3
kynmacapobre12 <macapobrekyn@gmail.com>
2024-11-11 10:41:42 +0800

    Add files via upload

---
+++ Prelim/Activity VI/MACAPOBRE_ACTIVITY_VI.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class MACAPOBRE_ACTIVITY_VI {
+    public static void main(String[]arg){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("How many boys in here");
+        int Sum = sc.nextInt();
+        System.out.println("How many girls in here");
+        int Difference = sc.nextInt();
+
+        int sum = Sum + Difference;
+        int Diff = Sum - Difference;
+        int product = Sum * Difference;
+        
+        System.out.println("sum = " +Sum);
+        System.out.println("diff = " +Diff);
+        System.out.println("product = " +product);
+         
+    
+
+
+        
+    }
+}
