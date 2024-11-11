71c1c1cf55d8aefdde254d111f70bcf6a645e1ce
Nathaniel-Lao <nathaniellao2005@gmail.com>
2024-11-11 10:14:19 +0800

    Add files via upload

---
+++ Midterm/Activity II/LAO_ACTIVITY_ II.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class LAO_ACTIVITY_II{
+     
+    public static void main(String[]args){
+        Scanner cs = new Scanner(System.in);
+        System.out.println("enter the age");
+        int age = cs.nextInt();
+
+        if (age <=11) {
+            System.out.println("child");
+
+        }
+        else if (age <=17) {
+            System.out.println("teen");
+
+        }    
+        else if (age <=64) {
+            System.out.println("adult");
+
+        }
+        cs.close();
+    }
+}
+
+
