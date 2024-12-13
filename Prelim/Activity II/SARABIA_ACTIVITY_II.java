d45b91e341fcd80b7dbefed02b913b9293dc093b
JC Martin K. Sarabia <sarabiajc233@gmail.com>
2024-12-13 14:28:25 +0800

    Update SARABIA_ACTIVITY_II.java

--- Prelim/Activity II/SARABIA_ACTIVITY_II.java
+++ Prelim/Activity II/SARABIA_ACTIVITY_II.java
@@ -6,7 +6,7 @@ public class SARABIA_ACTIVITY_II {
     public static void main(String[] args) {
         System.out.println("JC Martin K Sarabia");
         System.out.println(19);
-        System.out.println(21);
+        System.out.println(18);
         System.out.println(19-18);
     }  
 }
