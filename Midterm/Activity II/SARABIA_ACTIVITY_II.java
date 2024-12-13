3ca4f96ec055282037fb3729dd7d9e612c1ba69a
JC Martin K. Sarabia <sarabiajc233@gmail.com>
2024-12-13 12:31:01 +0800

    Add files via upload

---
+++ Midterm/Activity II/SARABIA_ACTIVITY_II.java
@@ -0,0 +1,12 @@
+/**
+ * SARABIA_ACTIVITY_II
+ */
+public class SARABIA_ACTIVITY_II {
+
+    public static void main(String[] args) {
+        System.out.println("JC Martin K Sarabia");
+        System.out.println(19);
+        System.out.println(21);
+        System.out.println(19-21);
+    }  
+}
