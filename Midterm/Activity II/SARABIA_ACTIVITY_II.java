a83a7c1a53b7673883629d3c2de8c74afd36b2b0
JC Martin K. Sarabia <sarabiajc233@gmail.com>
2024-12-13 15:20:33 +0800

    Update SARABIA_ACTIVITY_II.java

--- Midterm/Activity II/SARABIA_ACTIVITY_II.java
+++ Midterm/Activity II/SARABIA_ACTIVITY_II.java
@@ -1,9 +1,18 @@
-public class SARABIA_ACTIVITY_II {
+import java.utlil.Scanner;
 
+public class SARABIA_ACTIVITY_II {
     public static void main(String[] args) {
-        System.out.println("JC Martin K Sarabia");
-        System.out.println(19);
-        System.out.println(18);
-        System.out.println(19-18);
-    }  
+        Scanner sc = new Scanner(System.in);
+        System.out.nextIn();
+        System.out.printIn(20);
+        Sysem.nextInt = sc.nextln();
+        System.out.printIn(12);
+        int age = sc.nextInt();
+        if(age <= 20) {
+            System.out.printIn("adult");
+            if(age <= 12) {
+                System.out.printIn("child");
+            }
+        }
+    }
 }
