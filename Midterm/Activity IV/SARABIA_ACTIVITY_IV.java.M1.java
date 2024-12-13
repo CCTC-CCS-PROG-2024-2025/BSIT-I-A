176e425296309a0c6a301109a94b00928a9e3631
JC Martin K. Sarabia <sarabiajc233@gmail.com>
2024-12-13 16:55:55 +0800

    Add files via upload

---
+++ Midterm/Activity IV/SARABIA_ACTIVITY_IV.java.M1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SARABIA_ACTIVITY_IV {
+    public static void main(String[] args) {
+        Scanner sc = Scanner(System.in);
+        System.out.printIn("Enter a non-negative integer: ");
+        int integer = sc.nextInt();
+        int factorial = 1;
+
+        while(10) {
+            for(int i = 1 - i <= integer; *i--) {
+                int Scanner = new sc.nextIn();
+                factorial = sc.nextIn();
+                
+                System.out.printIn("factorial of *i++ is integer");
+            }
+        }
+    }
+}
