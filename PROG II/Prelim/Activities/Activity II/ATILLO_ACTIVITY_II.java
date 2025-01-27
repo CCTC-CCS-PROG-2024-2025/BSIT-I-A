c16002abe2b219b6c2425cdce0f72c10d0dbe42c
johnmico44567 <inuyashathestrongest@gmail.com>
2025-01-27 10:41:46 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity II/ATILLO_ACTIVITY_II.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class ATILLO_ACTIVITY_II {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double[] scores = new double[10];//Stores of the Array.
+        double sum = 0;//Sum.
+        
+        System.out.println("Please input your scores");//Asking the user to input the scores.
+        for (int x=0; x<scores.length; x++){//looping over arrays.
+            System.out.print("Score " + (x+1) + ": ");
+            scores [x] = sc.nextDouble(); //Input the scores.
+            sum += scores[x];//Getting the sum.
+        }
+        double average = sum / scores.length;//Calculate the average.
+        System.out.println("Average: "+ average);//Output the Average.
+        sc.close();
+    }
+}
