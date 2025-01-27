cf0e1769953eadfa29894f17ddd9a2015342c870
shofe ragodos <totskee12345@gmail.com>
2025-01-27 10:42:16 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity II/RAGODOS_ACTIVITY_II.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+    
+public class RAGODOS_ACTIVITY_II {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+            int[] scores = new int[10];
+                double sum = 0;
+                
+        System.out.println("ENTER 10 scores:");
+                for (int i = 0; i < scores.length; i++) {
+
+            System.out.print("Score " + (i + 1) + ": ");
+                scores[i] = scanner.nextInt();
+                    sum += scores[i];
+                }
+                double average = sum / scores.length;
+                System.out.println("THE AVERAGE IS: " + average);
+
+                    scanner.close();
+            }
+    }
