d204f1ea0ad4cba690f381930bd982e2218fcb84
Aj bastillas <archjunhelbastillas28@gmail.com>
2025-01-27 10:46:21 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity II/BASTILLAS_ACTIVITY_II.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class BASTILLAS_ACTIVITY_II {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] scores = new int[10];//Stores of the Array.
+        double sum = 0;
+        for (int x=0; x<scores.length; x++){
+            System.out.println("Score " + (x+1) + ": ");
+            int score = sc.nextInt();//Input the scores.
+            sum += score;//Getting the sum.
+        }
+        double average = sum / scores.length;//Calculate the average.
+        System.out.println("Average: "+ average);//Output the average.
+        sc.close();
+    }
+
+
+    }
