5e8b908cf9c567546e896a3c8148425ab1f87578
franciscomaricar <maricarfrancisco737@gmail.com>
2025-01-27 10:43:03 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity II/FRANCISCO_ACTIVITY_II.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class FRANCISCO_ACTIVITY_II {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[] scores = new double[10];
+        double sum=0;
+
+        System.out.println("Please Enter 10 Scores: ");
+
+        for (int i = 0; i < scores.length; i++) {
+            System.out.print("score " + (i + 1) +": ");
+            scores[i] = scanner.nextDouble(); 
+            sum += scores[i];
+        }
+        double average = sum / scores.length;
+        System.out.println("The average of the entered score is:" + average);
+
+        scanner.close();
+    }
+}
