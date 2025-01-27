6f2c199dcec8ed4ac6fb1d2974b2a5da314b545e
JC Martin K. Sarabia <sarabiajc233@gmail.com>
2025-01-27 10:40:54 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity II/SARABIA_ASSIGNMENT_ACTIVITY_II.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SARABIA_ASSIGNMENT_II {
+    public static void main(String[] args) {  
+        Scanner sc = new Scanner(System.in);
+        double[] scores = new double[10];
+
+        System.out.println("Enter 10 score: ");
+
+        for(int i = 0; i < scores.length; i++) {
+            System.out.println("Score" + (i + 1) + ": ");
+            scores[i] = sc.nextDouble();
+        }
+        double sum = 0;
+        for (double score : scores) {
+            sum += score;
+        }
+        double average = sum / scores.length;
+        System.out.println("the average score is: " + average);
+    }
+} 
