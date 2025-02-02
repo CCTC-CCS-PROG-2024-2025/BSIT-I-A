db07346d082f0e968cb9a1e0c6daf1ec3f33aa05
jeancentmigallen <migallenjeancentmay@gmail.com>
2025-02-02 20:03:37 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/MIGALLEN_ACTIVITY_I.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class MIGALLEN_ACTIVITY_I {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String [] names = new String [5];
+        
+ System.out.println("Please enter the names: ");
+        System.out.print("1. ");
+        names[0] = sc.nextLine();
+        System.out.print("2. ");
+        names[1] = sc.nextLine();
+        System.out.print("3. ");
+        names[2] = sc.nextLine();
+        System.out.print("4. ");
+        names[3] = sc.nextLine();
+        System.out.print("5. ");
+        names[4] = sc.nextLine();
+
+        System.out.print("Choose a name you want to display: ");
+        int choose = sc.nextInt(); 
+        int choose1 = choose - 1;
+        System.out.println("Name: " + (names[choose1])); 
+        sc.close();
+    }
+}
