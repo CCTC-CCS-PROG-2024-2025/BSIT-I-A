fe30228647586a5691db9e54ff8c6f4b45007980
Kyn S Macapobre <macapobrekyn@gmail.com>
2025-02-02 11:45:59 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity II/MACAPOBRE_ACTIVITY_II.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class MACAPOBRE_ACTIVITY_II {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double[] scores = new double[10];
+        double sum = 0;
+        
+        System.out.println("Kindly provide your scores: ");
+        for (int x=0; x<scores.length; x++){
+            System.out.print("Score " + (x+1) + ": ");
+            scores [x] = sc.nextDouble(); 
+            sum += scores[x];
+        }
+
+        double avg = sum / scores.length;
+        System.out.println("Average: "+ avg);
+        
+        sc.close();
+    }
+}
