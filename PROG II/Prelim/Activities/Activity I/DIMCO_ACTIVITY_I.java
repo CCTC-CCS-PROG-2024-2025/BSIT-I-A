a166d432d043db4725b73cffd9012a43209c1d3e
laurence45204 <dimcolaurnce@gmail.com>
2025-02-17 11:04:59 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/DIMCO_ACTIVITY_I.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class DIMCO_ACTIVITY_I {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String [] names = new String[5];
+
+        System.out.println("Please enter the names below: ");
+        System.out.print("1. ");
+        names[0] = sc.nextLine();
+
+        System.out.print("2. ");
+        names[1] = sc.nextLine();
+
+        System.out.print("3. ");
+        names[2] = sc.nextLine();
+
+        System.out.print("4. ");
+        names[3] = sc.nextLine();
+
+        System.out.print("5. ");
+        names[4] = sc.nextLine();
+        
+        System.out.print("elect a display name: ");
+        int choose = sc.nextInt(); 
+        int choose1 = choose - 1;
+        System.out.println("Name: " + (names[choose1]));
+        sc.close();
+    }
+}
