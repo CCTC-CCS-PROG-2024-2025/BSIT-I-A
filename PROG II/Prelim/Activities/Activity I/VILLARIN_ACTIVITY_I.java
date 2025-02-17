b3f9e8af7b693623090fe55ad356f283f55c4e97
noelvillarin <kalbopotoy123456789@gmail.com>
2025-02-17 11:12:12 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/VILLARIN_ACTIVITY_I.java
@@ -0,0 +1,25 @@
+mport java.util.Scanner;
+
+class VILLARIN_ACTIVITY_I {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String[] names = new String[5];
+        System.out.print("Kindly enter the names: ");
+        System.out.print("1. ");
+        names[0] = sc.nextLine();
+        System.out.print("2. ");
+        names[1] = sc.nextLine();
+        System.out.print("3. ");
+        names[2] = sc.nextLine();
+        System.out.print("4. ");
+        names[3] = sc.nextLine();
+        System.out.print("5. ");
+        names[4] = sc.nextLine();
+
+        System.out.println("Choose a name to display: ");
+        int choose = sc.nextInt();
+        int choose1 = choose - 1;
+        System.out.println("Name: " + names[choose1]);
+    }
+}
+    
