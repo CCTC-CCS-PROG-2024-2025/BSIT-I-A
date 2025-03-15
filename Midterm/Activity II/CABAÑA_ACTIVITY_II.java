a5c8c071025151cbf9bc08757de3f615fa814b8a
Perceval Cabaña <percevalcabana04@gmail.com>
2025-03-15 19:32:18 +0800

    Add files via upload

---
+++ Midterm/Activity II/CABAÑA_ACTIVITY_II.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class CABAÑA_ACTIVITY_II {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("input age: ");
+        int age = sc.nextInt();
+
+        if (age >= 0 && age <= 11) {
+            System.out.println("Child");
+        }
+        else if (age >= 12 && age <= 17) {
+            System.out.println("Teen");
+        }
+        else if (age >= 18 && age <= 64) {
+            System.out.println("Adult");
+        }
+        else if (age <0) {
+            System.out.println("Naapaka sa itlog sa imong PAPA");
+        }
+    }
+}
