a9d70d8919f5bc4dc3e60e5f0370d0fe50801831
ailene-vi <ailenevilla3@gmail.com>
2025-03-17 10:43:45 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/VILLA_A_ACTIVITY_I.java
@@ -0,0 +1,18 @@
+public class VILLA_A_ACTIVITY_I {
+    
+    public static void aboutMe() {
+        
+        String name = "Ailene D. Villa";
+        int age = 19; 
+        String address = "Mabini 1, Tajao, Pinamungajan, Cebu";
+
+        
+        System.out.println("My name is " + name + ", I am " + age + " years old, and I live in " + address + ".");
+    }
+
+    
+    public static void main(String[] args) {
+        
+        aboutMe();
+    }
+}
