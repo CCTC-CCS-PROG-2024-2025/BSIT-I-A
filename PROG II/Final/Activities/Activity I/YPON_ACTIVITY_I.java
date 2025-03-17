b4dd3b6891835b4f34ebb1857e7ae05458b9c56b
Kyla Mae Ypon <kylayu811@gmail.com>
2025-03-17 10:53:59 +0800

    Update YPON_ACTIVITY_I.java

--- PROG II/Final/Activities/Activity I/YPON_ACTIVITY_I.java
+++ PROG II/Final/Activities/Activity I/YPON_ACTIVITY_I.java
@@ -4,7 +4,7 @@ class YPON_ACTIVITY_I {
     int age = 19;
     String address = "Mainggit Sto. Nino, Toledo City, Cebu.";
 
-    System.out.print("Hello my name is " + name + "." + " I'm " + age + " years old. " + "I live in " + address);
+    System.out.print("Hello, my name is " + name + "." + " I'm " + age + " years old. " + "I live in " + address);
   }
 
   public static void main(String[] args) {
