64722d2af7666203e61da1b4a7f7b29383eea53e
Daisyree Bacalla <bacalladaisyree4@gmail.com>
2025-03-17 10:50:18 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/BACALLA_ACTIVITY_I.java
@@ -0,0 +1,15 @@
+class BACALLA_ACTIVITY_I {
+    static void aboutMe() {
+      System.out.println("My name is Daisyree Bacalla");
+      System.out.println("I am 18 years old");
+      System.out.println("I lived in Buanoy Balamban Cebu");
+ 
+   }
+   public static void main(String[] args) {
+        aboutMe();
+   }
+ 
+ }
+ 
+ 
+ 
