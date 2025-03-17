3edb36e13931ecb6a1d4306aab788d7204c40389
Clint Caserial <clintcaserial3@gmail.com>
2025-03-17 10:42:29 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/CASERIAL_ACTIVITY_I.java
@@ -0,0 +1,16 @@
+public class CASERIAL_ACTIVITY_I {
+
+    
+    public static void aboutMe (String name, int age, String address) {
+        System.out.println("\nMy name is " + name + ". I am " + age + " years old. I live at " + address + ".\n");
+    }
+
+    public static void main(String[] args) {
+        String name = "James Ivan";
+        int age = 19;
+        String address = "Carmen, Toledo City";
+
+        
+        aboutMe(name, age, address);
+    }
+}
