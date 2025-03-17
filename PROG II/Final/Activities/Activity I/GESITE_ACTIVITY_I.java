4ca209b39ac43832f1788331beb4df4f8683c7ae
JUN REV GESITE <imsoofansay1@gmail.com>
2025-03-17 10:40:51 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/GESITE_ACTIVITY_I.java
@@ -0,0 +1,13 @@
+public class GESITE_ACTIVITY_I{
+
+    public static void AboutMe() {
+        String name = " Jun Rev A. Gesite";
+        int age = 20;
+        String address = "Poblacion, T.C.";
+        System.out.println("My name is" + name + "." + "I am " + age + " years old." + "I live at " + address);
+
+    }
+    public static void main (String[] args) {
+        AboutMe();
+    }
+}
