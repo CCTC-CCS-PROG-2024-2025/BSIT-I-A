b990a07bdc35b4d7bf0e1e1174d4ee99ae520968
Noely Jed E. Arcay <drixgael@gmail.com>
2025-03-17 10:39:23 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/ARCAY_ACTIVITY_1.java
@@ -0,0 +1,12 @@
+class ARCAY_ACTIVITY_I{
+    static void aboutMe(){
+        String name = "Noely Jed E. Arcay";
+        int age = 18;
+        String address = "Magic, Poblacion, Toledo City, Cebu.";
+
+        System.out.print("Hello my name is "+ name + "."+" I'm "+ age + " years old. "+" And I live in "+ address);
+    }
+    public static void main(String[] args){
+        aboutMe();
+    }
+}
