9281567efefe54c8c882f056f73d3adb9a35c1eb
john tracel demotor <demotorjohntracel@gmail.com>
2025-03-17 10:45:29 +0800

    Add files via upload

---
+++ Prelim/Activity II/class DEMOTOR_ACTIVITY_II{.java
@@ -0,0 +1,12 @@
+class DEMOTOR_ACTIVITY_II{
+    public static void main (String[] args) {
+        System.out.println("Noely Jed E. Arcay");
+        System.out.println(17);
+        System.out.println(18);
+        System.out.println(17-18);
+    }
+}
+
+
+
+
