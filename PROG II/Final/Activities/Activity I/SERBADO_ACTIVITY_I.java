91b5718477b6df5ed3a7270ef73b9e0e51e96e5b
Princess Mae R. Serbado <serbadoprincessmaer@gmail.com>
2025-03-17 10:54:24 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/SERBADO_ACTIVITY_I.java
@@ -0,0 +1,15 @@
+class SERBADO_ACTIVITY_I {
+    static void aboutMe() {
+      System.out.println("My name is Princess Mae Serbado");
+      System.out.println("I am 19 years old");
+      System.out.println("I lived in Pondol Balamban Cebu");
+ 
+   }
+   public static void main(String[] args) {
+        aboutMe();
+   }
+ 
+ }
+ 
+ 
+ 
