88ab24760c7c956defa269c3b0110dd31de4738f
Nathaniel-Lao <nathaniellao2005@gmail.com>
2025-03-17 10:44:46 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/LAO_ACTIVITY_1_PROG2.java
@@ -0,0 +1,18 @@
+
+class LAO_ACTIVITY_I{
+
+    static void AboutMe (){
+        String name = "Nathaniel Alexis P. Lao";
+        int age = 19;
+        String address = "Poblacion Pinamungajan Cebu";
+
+        System.out.println("My name is" + name + "Im" + age + "Years old" +"i am from" + address);
+    }
+    public static void main(String[]args) {
+        AboutMe();
+        System.out.println ();
+    }
+}
+
+
+
