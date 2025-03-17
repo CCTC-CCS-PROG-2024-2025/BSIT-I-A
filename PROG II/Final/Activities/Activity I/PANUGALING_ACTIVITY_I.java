8a79b522af66800512ca979bbaf6b4b27d4ae793
James Ivan Panugaling <jamesivanipis@gmail.com>
2025-03-17 10:41:43 +0800

    PANUGALING_ACTIVITY_I.java

--- PROG II/Final/Activities/Activity I/PANUGALING_ACTIVITY_I.java
+++ PROG II/Final/Activities/Activity I/PANUGALING_ACTIVITY_I.java
@@ -1,16 +1,15 @@
 public class PANUGALING_ACTIVITY_I {
 
     
-    public static void aboutMe (String name, int age, String address) {
+    public static void aboutMe () {
+        String name = "James Ivan";
+        int age = 19;
+        String address = "Carmen, Toledo City";
         System.out.println("\nMy name is " + name + ". I am " + age + " years old. I live at " + address + ".\n");
     }
 
     public static void main(String[] args) {
-        String name = "James Ivan";
-        int age = 19;
-        String address = "Carmen, Toledo City";
-
         
-        aboutMe(name, age, address);
+        aboutMe ();
     }
 }
