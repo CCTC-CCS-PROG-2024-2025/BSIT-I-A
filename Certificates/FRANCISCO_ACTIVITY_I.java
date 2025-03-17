763b270422031cf4897618f659b2d81b5119fba7
franciscomaricar <maricarfrancisco737@gmail.com>
2025-03-17 10:50:26 +0800

    Add files via upload

---
+++ Certificates/FRANCISCO_ACTIVITY_I.java
@@ -0,0 +1,12 @@
+class FRANCISCO_ACTIVITY_I{
+    static void aboutMe() {
+       System.out.println("My name is Maricar Francisco.");
+       System.out.println("I am 19 years old.");
+       System.out.println("I lived in Abucayan , Balamban , Cebu");
+       
+    }
+    public static void main(String[] args) {
+        aboutMe();
+    }
+ 
+}
