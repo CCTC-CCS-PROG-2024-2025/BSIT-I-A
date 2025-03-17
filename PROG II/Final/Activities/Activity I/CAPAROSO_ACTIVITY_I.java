89bfdeedad7e6fb9d8b43e5b86f8c21c07aa7f1e
Nilfa Mica Caparoso <caparosonilfa@gmail.com>
2025-03-17 10:43:48 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/CAPAROSO_ACTIVITY_I.java
@@ -0,0 +1,18 @@
+public class CAPAROSO_ACTIVITY_I {
+    
+    static void aboutMe() {
+        
+        String name = "Nilfa";
+        int age = 21; 
+        String address = "Balamban";
+
+        
+        System.out.println("My name is " + name + ", I am " + age + " years old, and I live in " + address + ".");
+    }
+
+    
+    public static void main(String[] args) {
+        
+        aboutMe();
+    }
+}
