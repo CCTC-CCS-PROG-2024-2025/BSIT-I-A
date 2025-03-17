30e9b5f999dc0c7b44ccabca709647af234ee8c3
Kyn S Macapobre <macapobrekyn@gmail.com>
2025-03-17 10:49:40 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/MACAPOBRE_ACTIVITY_I.java
@@ -0,0 +1,12 @@
+public class MACAPOBRE_ACTIVITY_I {
+    
+    public static void aboutMe() {
+        String name = "KYN S. MACAPOBRE";
+        int age = 22;
+        String address = "Purok Aroma, Dumlog, Toledo City, Cebu";
+        System.out.println("My name is " + name + ". I am " + age + " years old. I live in " + address + ".");
+    }
+    public static void main(String[] args) {
+        aboutMe();
+    }
+}
