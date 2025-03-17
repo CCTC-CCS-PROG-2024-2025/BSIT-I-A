794ded897077dc8e7c3ae53782031cb7cd8f73fa
Perceval Cabaña <percevalcabana04@gmail.com>
2025-03-17 10:42:54 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/CABANA_ACTIVITY_I.java
@@ -0,0 +1,12 @@
+class CABANA_ACTIVITY_I {
+    public static void main(String [] args ) {
+        aboutMe();
+    }
+    public static void aboutMe() {
+        String name = "Perceval ";
+        int age = 20;
+        String address = "Lower Media Once, Toledo City";
+
+        System.out.println("My name is " + name + "I am " + age + " years old " + "Live in " + address);
+    }
+}
