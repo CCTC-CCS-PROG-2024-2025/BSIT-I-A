39f4a3f6d314d61d79a771139c2dd9bf2e902c9a
lindsaybellekangkee09 <lindsaybellek@gmail.com>
2025-03-17 10:57:25 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/KANGKEE_ACTIVITY_I.java
@@ -0,0 +1,12 @@
+class KANGKEE_ACTIVITY_I{
+    static void aboutMe() {
+       System.out.println("My name is Lindsay Belle Fankrasha Q. Kangkee.");
+       System.out.println("I am 19 years old.");
+       System.out.println("I lived in Abucayan , Balamban , Cebu");
+       
+    }
+    public static void main(String[] args) {
+        aboutMe();
+    }
+ 
+}
