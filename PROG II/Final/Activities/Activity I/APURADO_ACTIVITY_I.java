a56f1aa2f787378c869882d02f33d5ff89fb739e
Calvin John Panla-an <166713172+Calvon33123@users.noreply.github.com>
2025-03-17 10:50:35 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/APURADO_ACTIVITY_I.java
@@ -0,0 +1,29 @@
+
+class APURADO_ACTIVITY_I {
+    static void aboutMe(){
+        String name = "Jefer A. Apurado";
+        int age = 19;
+        String adress = "Ladahan Toledo City";
+
+ 
+        System.out.println("Hello my name is " + name + ". I am " + age + " years old. " + "I live in " + adress + ".");
+
+
+        
+    
+    }
+     
+    public static void main(String[]args){
+
+    aboutMe();
+    }
+
+     
+     
+
+
+     
+
+    
+}
+ 
