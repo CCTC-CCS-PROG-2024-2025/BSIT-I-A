01d8b86d135d1b214b2ee974ec3712d992412b48
JC Martin K. Sarabia <sarabiajc233@gmail.com>
2025-03-17 10:58:28 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/SARABIA_ACTIVITY_I.java
@@ -0,0 +1,12 @@
+public class SARABIA_ACTIVITY_I {
+    
+    public static void aboutMe() {
+        String name = "JC MARTIN K. SARABIA";
+        int age = 20;
+        String address = "Poblacion, Toledo City, Cebu";
+        System.out.println("My name is " + name + ". I am " + age + " years old. I live in " + address + ".");
+    }
+    public static void main(String[] args) {
+        aboutMe();
+    }
+}
