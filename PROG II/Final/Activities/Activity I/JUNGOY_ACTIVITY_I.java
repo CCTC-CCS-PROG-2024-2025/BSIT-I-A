5dd4825afac4056dea9bd9b4f12811bcf326c9ce
Ericka18791 <erickajungoy41@gmail.com>
2025-03-17 10:45:03 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/JUNGOY_ACTIVITY_I.java
@@ -0,0 +1,18 @@
+class JUNGOY_ACTIVTY_I{
+    
+        public static void aboutMe() {
+            
+            String name ="Ericka";
+            int age = 20 ;
+            String address = "Toledo City";
+    
+            
+            System.out.println("My name is " + name + ", I am " + age + " years old, and I live in " + address + ".");
+        }
+    
+        
+        public static void main(String[] args) {
+            
+            aboutMe();
+        }
+    }
