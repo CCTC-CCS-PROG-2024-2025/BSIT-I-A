be75960b2f3d41b75c2a1f7d8908d67d20160f0d
Fluximity <gogleuser03@gmail.com>
2025-03-17 10:46:55 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/CANEZRAES_ACTIVITY_I.java
@@ -0,0 +1,12 @@
+class CANEZRAES_ACTIVITY_I{
+    static void aboutMe(){
+        String NAME = "Gabriel Cañezares";
+        int AGE = 18;
+        String ADDRESS = "Tanibag, Pinamungajan, Cebu";
+
+        System.out.print("Hello my name is "+ NAME + "."+" I'm "+ AGE + " years old. "+" And I live in "+ ADDRESS);
+    }
+    public static void main(String[] args){
+        aboutMe();
+    }
+}
