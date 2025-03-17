98a9ba1b68cad389fab16a2fe37ce195e87f4085
Lloyd Tito <notlloydtito@gmail.com>
2025-03-17 10:40:00 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/TITO_ACTIVITY_I.java
@@ -0,0 +1,12 @@
+public class TITO_ACTIVITY_I {
+    
+    public static void aboutMe() {
+        String name = "Lloyd N. Tito";
+        int age = 20;
+        String address = "Luray I, Toledo City, Cebu";
+        System.out.println("My name is " + name + ". I am " + age + " years old. I live in " + address + ".");
+    }
+    public static void main(String[] args) {
+        aboutMe();
+    }
+}
