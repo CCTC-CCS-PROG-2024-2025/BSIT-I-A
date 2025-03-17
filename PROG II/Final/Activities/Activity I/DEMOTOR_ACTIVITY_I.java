1ee98cd7eb699c204a177e822e8a516337831965
john tracel demotor <demotorjohntracel@gmail.com>
2025-03-17 10:40:47 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/DEMOTOR_ACTIVITY_I.java
@@ -0,0 +1,12 @@
+public class DEMOTOR_ACTIVITY_I {
+    
+    public static void aboutMe() {
+        String name = "John Tracel M. Demotor";
+        int age = 18;
+        String address = "Camanggahan II, Buanoy, Balamban City, Cebu";
+        System.out.println("My name is " + name + ". I am " + age + " years old. I live in " + address + ".");
+    }
+    public static void main(String[] args) {
+        aboutMe();
+    }
+}
