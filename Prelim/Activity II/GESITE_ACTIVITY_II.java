7cb1ca7d00a08284c34d47498e5ae14080aac955
JUN REV GESITE <imsoofansay1@gmail.com>
2025-03-17 10:46:03 +0800

    Add files via upload

---
+++ Prelim/Activity II/GESITE_ACTIVITY_II.java
@@ -0,0 +1,8 @@
+class GESITE_ACTIVITY_II{
+    public static void main (String[] args) {
+        System.out.println("Noely Jed E. Arcay");
+        System.out.println(17);
+        System.out.println(18);
+        System.out.println(17-18);
+    }
+}
