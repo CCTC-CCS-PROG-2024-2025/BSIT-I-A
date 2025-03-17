291e135048a8b642796c7fc230fe29c0cfbf1e34
L3BR0NJ4M3S <lebronjamesabong@gmail.com>
2025-03-17 10:44:02 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/ABONG_ACTIVITY_I.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class ABONG_ACTIVITY_1 {
+    static void aboutMe () {
+        String name = "Lebron James D. Abong";
+        int age = 19;
+        String address = "Pandacan Pinamungajan Cebu";
+        System.out.println("My Names is " + name + " Im " + age + " Years old " + " and Im from " + address +".");
+    }
+    public static void main (String [] args) {
+            aboutMe ();
+            System.out.println ();
+    }
+
+}
