310ea7e939f72a287cca043a3b6fb3b6ed8fd5dc
Kyla Mae Ypon <kylayu811@gmail.com>
2025-03-17 10:45:18 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/YPON_ACTIVITY_I.java
@@ -0,0 +1,13 @@
+class YPON_ACTIVITY_I {
+  static void aboutMe() {
+    String name = "Kyla Mae Ypon";
+    int age = 19;
+    String address = "Mainggit Sto. Nino, Toledo City, Cebu.";
+
+    System.out.print("Hello my name is " + name + "." + " I'm " + age + " years old. " + "And I live in " + address);
+  }
+
+  public static void main(String[] args) {
+    aboutMe();
+  }
+}
