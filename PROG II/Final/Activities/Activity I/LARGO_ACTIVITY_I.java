c690a5f06a142e2c4b08c4a544bbf120a97fd367
Jake Largo <jakelargo99p@gmail.com>
2025-03-17 10:54:59 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/LARGO_ACTIVITY_I.java
@@ -0,0 +1,16 @@
+class LARGO_ACTIVITY_I {
+    public static void main(String[] args) {
+        AboutMe();
+    }
+    static void AboutMe() {
+        String Name ="Jake L. largo.";
+        int Age = 19;
+        String Address ="Sipaway, Luray II Toledo City.";
+
+
+
+        System.out.println(" My name Is " + Name + " My age Is " + Age + " And Im from " + Address);
+
+    
+    }
+}
