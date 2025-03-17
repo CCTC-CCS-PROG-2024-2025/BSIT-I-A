05c8f66964b6a08abe966a97a0def14866b2fe11
RyanStepGenz <ryanjune111@gmail.com>
2025-03-17 10:41:34 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/LACANGLACANG_ACTIVITY_I.java
@@ -0,0 +1,12 @@
+public class LACANGLACANG_ACTIVITY_I {
+    
+    public static void aboutMe() {
+        String name = "Ryanjune G. Lacang-Lacang";
+        int age = 18;
+        String address = "Bato, Toledo City, Cebu";
+        System.out.println("My name is " + name + ". I am " + age + " years old. I live in " + address + ".");
+    }
+    public static void main(String[] args) {
+        aboutMe();
+    }
+}
