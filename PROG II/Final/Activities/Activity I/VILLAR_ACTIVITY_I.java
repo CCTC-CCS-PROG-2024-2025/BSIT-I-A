20790a2c23c22a9ac881406af6bb67106844518b
Villar, Edison P. <167503476+nommuu@users.noreply.github.com>
2025-03-17 10:43:20 +0800

    VILLAR_ACTIVITY_I.java

--- PROG II/Final/Activities/Activity I/VILLAR_ACTIVITY_I.java
+++ PROG II/Final/Activities/Activity I/VILLAR_ACTIVITY_I.java
@@ -4,13 +4,13 @@ class VILLAR_ACTIVITY_I {
     static void aboutME() {
         String name = "Edison P. Villar"; 
         int age = 19;
-        String add = "Poblacion Pinamungajan, Cebu";
-        System.out.println("My name is " + name + "." + " I am " + age + " years old. and I live in " + add + ".");
+        String addr = "Poblacion Pinamungajan, Cebu";
+        System.out.println("My name is " + name + "." + " I am " + age + " years old. and I live in " + addr + ".");
         
     }
     public static void main(String[] args) {
         aboutME();
-        System.out.println();
+        
     }
 
 
