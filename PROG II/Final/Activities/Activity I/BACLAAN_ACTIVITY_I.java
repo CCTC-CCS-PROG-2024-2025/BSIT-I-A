15e45613a8a87ba094eb228da8809f6f81a3103b
Harold Jel baclaan <baclaanharold401@gmail.com>
2025-03-17 10:57:34 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/BACLAAN_ACTIVITY_I.java
@@ -0,0 +1,12 @@
+class BACLAAN_ACTIVITY_I {
+      public static void  main(String []args) {
+         aboutme();
+      }
+      public static void aboutme() {
+        String name = "Harold ";
+        int age = 19;
+        String address = "Tinahas Media Once, Toledo City";
+        System.out.println( "My name is " + name + "i am" + age + "years old" + "Live in " + address);
+        
+    }
+}
