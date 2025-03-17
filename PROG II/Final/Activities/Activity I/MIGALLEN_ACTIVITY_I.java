73014f58cc2f046799caf8059c56d3d0b790266d
jeancentmigallen <migallenjeancentmay@gmail.com>
2025-03-17 10:49:50 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/MIGALLEN_ACTIVITY_I.java
@@ -0,0 +1,10 @@
+class MIGALLEN_ACTIVITY_I {
+    public static void main(String [] args) {
+        String name = "Jeancent";
+        int age = 19;
+        String address = "Abucayan Balamban Cebu";
+        
+        System.out.println("My name is " + name + " I am " + age +" years old, and i live at " + address + ".");
+        
+    }
+}
