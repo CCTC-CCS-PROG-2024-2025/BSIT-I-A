06b93a115e969cfb54057414338f2f09bd228597
johnmico44567 <inuyashathestrongest@gmail.com>
2025-03-17 10:48:06 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/ATILLO_ACTIVITY_I.java
@@ -0,0 +1,18 @@
+
+class ATILLO_ACTIVITY_I {
+    static void aboutMe(){
+        String name ="John Mico F. Atillo.";
+        int age = 19;
+        String adress = "Sipaway Luray II Toledo City.";
+
+
+        System.out.println("Hello my name is " + name + " I am " + age +  ". I live in " + adress);
+    
+    }
+
+    public static void main (String[]args){
+
+    aboutMe();
+
+    }
+}
