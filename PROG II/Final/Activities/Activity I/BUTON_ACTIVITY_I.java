75335d2c88f4c0cc0d9f72655d897f0d24eb77fc
JEAN MAICA <jeanbuton754@gmail.com>
2025-03-17 11:00:10 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/BUTON_ACTIVITY_I.java
@@ -0,0 +1,17 @@
+class BUTON_ACTIVITY_I {
+    static void aboutMe() {
+        String name = "Jean Maica Cris C. Buton";
+        int age = 19;
+        String Address = "Brgy. Ilihan, Toledo City, Cebu";
+        System.out.println(" My name is " + "name"+ "Im" + age + "Years old" + "and Im from" + Address + ".");
+       
+        
+        }
+
+        public static void main (String[] args) {
+            aboutMe();
+        
+    }
+
+
+    }
