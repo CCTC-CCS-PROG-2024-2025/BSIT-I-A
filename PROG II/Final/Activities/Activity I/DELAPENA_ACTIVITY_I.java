f4da8eca5990a528084e5243cc30bd819fc561a4
pookie-bear11 <ethangabrieldelapena@gmail.com>
2025-03-17 10:52:06 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/DELAPENA_ACTIVITY_I.java
@@ -0,0 +1,14 @@
+class DELAPENA_ACTIVITY_I {
+    static void aboutMe(){
+        String name = "Ethan Gabriel M. Dela Peña";
+        int age = 19;
+        String adress = "Lawis, Luray 2 Toledo City";
+
+        System.out.println("My name is " + " "+ name + "," + " I am " + age + " years old "+ "," + " " + "I live in" + " "+ adress+".");
+        
+    }
+    public static void main(String[] args) {
+        aboutMe();
+    }
+
+}
