d6a671a5edc19d2bf26186219ced5c4b2796b4fc
Calvin John Panla-an <166713172+Calvon33123@users.noreply.github.com>
2025-03-17 10:39:28 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/PANLAAN_ACTIVITY_I.java
@@ -0,0 +1,17 @@
+
+class PANLAAN_ACTIVITY_I{
+
+    static void aboutMe(){
+        String name = "Calvin John C. Panla-an";
+        int age = 19;
+        String adress = "Pandacan, Pinamungajan, Cebu";
+
+        System.out.println("Hello my name is " + name + ". I am " + age + " years old. " + "I live in " + adress + ".");
+
+    }
+    public static void main (String[] args){
+    
+        aboutMe();
+
+    }
+ }
