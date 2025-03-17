faa65632e53773e07ae89359702895bdcbda59d7
Kazcel Siarot <siarotkazcel23@gmail.com>
2025-03-17 10:43:57 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/SIAROT_ACTIVITY_I.java
@@ -0,0 +1,18 @@
+ class SIAROT_ACTIVITY_I{
+    
+    public static void aboutMe() {
+        
+        String name = "Kazcel";
+        int age = 21; 
+        String address = "Balamban";
+
+        
+        System.out.println("My name is " + name + ", I am " + age + " years old, and I live in " + address + ".");
+    }
+
+    
+    public static void main(String[] args) {
+        
+        aboutMe();
+    }
+}
