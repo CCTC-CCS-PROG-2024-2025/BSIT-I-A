be4ee5f91e89cdd5ba549636fdb8b0393f64bab3
Sendic Jacob M. Alqueza <sendic.alqueaza08@gmail.com>
2025-03-17 10:50:41 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/ALQUEZA_ACTIVITY_I.java
@@ -0,0 +1,14 @@
+class ALQUEZA_ACTIVITY_I {
+    static void aboutMe(){
+        String name = "Sendic JAcob Alqueza";
+        int age = 19;
+        String adress = "Awihao Toledo city, Cebu";
+
+        System.out.println("My name is " +" "+ name + "," + " I am " + age + "  years old "+ "," + "I live in" + " "+ adress);
+        
+    }
+    public static void main(String[] args) {
+        aboutMe();
+    }
+
+}
