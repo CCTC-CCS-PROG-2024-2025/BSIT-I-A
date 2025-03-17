1b422a3fc4797e5acfc9a55f9195246271e1ec1a
FidelVilla02 <villafidel96@gmail.com>
2025-03-17 10:43:42 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/VILLA_F_ACTIVITY_I.java
@@ -0,0 +1,14 @@
+
+public class VILLA_F_ACTIVITY_I {
+    static void aboutMe() {
+        String name = "Fidel Nino S. Villa";
+        int age = 22;
+        String address = "Cambuhawe Balamban Cebu";
+        System.out.println("My name is " + name + ". I am " + age + " years old. I live in " + address + ".");
+
+  }
+  public static void main(String[] args) {
+    aboutMe();
+
+    }
+}
