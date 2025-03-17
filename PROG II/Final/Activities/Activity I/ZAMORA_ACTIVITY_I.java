1174fd3993d8076949d2125ffb5208d916548ed9
danielleandreizamora1 <kizukishimada@gmail.com>
2025-03-17 10:55:46 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/ZAMORA_ACTIVITY_I.java
@@ -0,0 +1,14 @@
+class ZAMORA_ACTIVITY_I {
+    static void aboutMe(){
+        String name = "DANIELLE ANDREI ZAMORA";
+        int age = 19;
+        String adress = "Lawis, Luray 2 Toledo City";
+
+        System.out.println("My name is " +" "+ name + "," + " I am " + age + "  years old "+ "," + "I live in" + " "+ adress);
+        
+    }
+    public static void main(String[] args) {
+        aboutMe();
+    }
+
+}
