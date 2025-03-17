989e41daa9efd34b93d94290388998a7834d8222
nenakassandraencabo <nenakassandraencabo@gmail.com>
2025-03-17 10:48:15 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/ENCABO_ACTIVITY_I.java
@@ -0,0 +1,15 @@
+class ENCABO_ACTIVITY_I {
+        public static void main(String[] args) {
+            aboutMe();
+        }
+        public static void aboutMe(){
+            String name = "Nena Kassandra";
+            int age = 18;
+            String address = "Abucayan, Balamban, Cebu";
+
+            System.out.println("My name is "+ name + " I am "+ age + " years old "+ "Live in" + address);
+
+        }  
+
+
+        }
