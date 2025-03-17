ffb6cb09e0068f96eb09bbdc9e31a90962356c8f
nhoviecamingao20 <nhoviecamingao25@gmail.com>
2025-03-17 11:00:47 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/CAMINGAO_FINALS_I.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class CAMINGAO_ACTIVITY_I {
+    static void aboutMe () {
+        String name = "Nhovie Camingao";
+        int age = 19;
+        String address = "Harden Ilihan Toledo City, Cebu";
+        System.out.println("My name is " + name + " Im " + age + " Years old" + " and im from" + address + ".");
+    }
+    public static void main (String[] args){
+        aboutMe ();
+        System.out.println ();
+    }
+
+}
+    
+       
