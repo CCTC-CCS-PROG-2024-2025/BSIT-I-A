59d61a2a40c05918f9240d5adc01b6cb802aa9c8
Aj bastillas <archjunhelbastillas28@gmail.com>
2025-03-17 11:00:07 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/BASTILLAS_ACTIVITY_I.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BASTILLAS_ACTIVITY_I{
+   static void aboutMe() {
+    String name = "Archjun Bastillas";
+    int age= 19;
+    String Address = " Harden Ilihan";
+    System.out.println("My name is " + name +" Im " + age +" Years old "+" and Im from "+ Address + " "  ); 
+
+    }
+    public static void main (String[] args) {
+        aboutMe();
+    }
+        
+}
