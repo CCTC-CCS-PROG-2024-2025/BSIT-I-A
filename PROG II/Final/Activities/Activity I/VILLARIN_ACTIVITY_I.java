4a89d97e3b0c0e617fa32b6d100ff0b3bad50a11
noelvillarin <kalbopotoy123456789@gmail.com>
2025-03-17 11:00:35 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/VILLARIN_ACTIVITY_I.java
@@ -0,0 +1,16 @@
+
+public class VILLARIN_ACTIVITY_I {
+    static void aboutMe() {
+        String name ="Noel M, Villarin";
+        int age = 20;
+        String address ="Bato Toledo City";
+
+        System.out.println("My name is " + name + ", I am" + age + " years old, and I live at " + address + ".");
+    }
+    
+
+    public static void main(String[]args) {
+
+        aboutMe();
+    }
+}
