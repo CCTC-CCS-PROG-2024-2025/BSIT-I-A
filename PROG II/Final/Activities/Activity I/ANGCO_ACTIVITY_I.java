7ace4a82cb4920fc0c4b8fc4b9c601b0aac5b27c
Lablab <laberneangco123@gmail.com>
2025-03-17 10:39:27 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/ANGCO_ACTIVITY_I.java
@@ -0,0 +1,15 @@
+public class ANGCO_ACTIVITY_I{
+public static void aboutMe(String name, int age, String address){
+    System.out.println("Hello, my name is: " + name + ". and I am: " + age + " years old. I live in: " + address + ".");
+}
+    public static void main(String[] args){
+String name ="Laberne Angco";
+int age = 20;
+String address = "Toledo City, Cebu";
+
+aboutMe(name, age, address);
+ }
+  }
+
+
+
