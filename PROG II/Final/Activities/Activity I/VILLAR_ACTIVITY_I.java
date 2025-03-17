3158022b99a48eefc60da0ee18fcf58441119074
Villar, Edison P. <167503476+nommuu@users.noreply.github.com>
2025-03-17 10:39:26 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/VILLAR_ACTIVITY_I.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class VILLAR_ACTIVITY_I {
+    static void aboutME() {
+        String name = "Edison P. Villar"; 
+        int age = 19;
+        String add = "Poblacion Pinamungajan, Cebu";
+        System.out.println("My name is " + name + "." + " I am " + age + " years old. and I live in " + add + ".");
+        
+    }
+    public static void main(String[] args) {
+        aboutME();
+        System.out.println();
+    }
+
+
+}
