93bfe63c32dbcd371c6a5d110ba0c48bfa3a1b9e
rjean-manyatay <patambagrjean@gmail.com>
2025-03-17 10:56:21 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/MANAYTAY _FINALS_ACTIVITY _I.java
@@ -0,0 +1,18 @@
+
+public class MANAYTAY_ACTIVITY_I {
+    static void aboutMe() {
+        String name = "R-jean P. Manaytay";
+        int age = 20;
+        String address = "Khinner Toledo City";
+
+        System.out.println("My name is " + ", I am" + age + " years old, and I live at " + address + ".");
+    }
+
+
+    public static void main(String[]args) {
+        aboutMe();
+    }
+}
+       
+
+
