9dda4db56d2cd0c5cd8dfc98a45bec6e3631f632
Jumar Dizon <swordego2019@gmail.com>
2025-03-18 17:44:00 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity I/DIZON_ACTIVITY_I.java
@@ -0,0 +1,12 @@
+class DIZON_ACTIVITY_I {
+    static void aboutMe() {
+        String name = "Jumar S. Dizon";
+        int age = 27;
+        String address = "Lawis, Sto Niño Mainggit, Toledo City, Cebu.";
+
+        System.out.print("Hello my name is "+ name + "."+" I'm "+ age + " years old. "+" And I live in "+ address);
+    }
+    public static void main(String[] args){
+        aboutMe();
+    }
+}
