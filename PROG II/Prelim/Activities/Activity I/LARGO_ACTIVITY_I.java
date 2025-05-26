2562da6d0c25c8afaf72ec51764eb69eed2f538a
Jake Largo <jakelargo99p@gmail.com>
2025-05-26 15:20:44 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/LARGO_ACTIVITY_I.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class LARGO_ACTIVITY_I {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String [] names = new String[5];
+
+        System.out.println("Enter the names: ");
+        System.out.print("1. ");
+        names[0] = sc.nextLine();
+
+        System.out.print("2. ");
+        names[1] = sc.nextLine();
+
+        System.out.print("3. ");
+        names[2] = sc.nextLine();
+
+        System.out.print("4. ");
+        names[3] = sc.nextLine();
+
+        System.out.print("5. ");
+        names[4] = sc.nextLine();
+        
+        System.out.print("Choose to display name/s: ");
+
+        int choose = sc.nextInt(); 
+        int choose1 = choose - 1;
+
+        System.out.println("Name: " + (names[choose1]));
+        sc.close();
+    }
+}
